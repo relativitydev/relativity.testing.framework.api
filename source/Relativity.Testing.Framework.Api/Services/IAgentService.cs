25a551ce06abfc0cbc7dd30fd3f993c198dd2e90
Klaudia Knafel <klaudia.knafel@relativity.com>
2021-06-21 16:33:37 +0200

    [RTF-1271] Adding examples to IAgentService docs.

--- source/Relativity.Testing.Framework.Api/Services/IAgentService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IAgentService.cs
@@ -7,6 +7,12 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the agent API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// ...
+	/// _agentService = relativityFacade.Resolve&#60;IAgentService&#62;();
+	/// </code>
+	/// </example>
 	public interface IAgentService
 	{
 		/// <summary>
@@ -14,30 +20,60 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="entity">The entity to create.</param>
 		/// <returns>The created entity.</returns>
+		/// <example>
+		/// <code>
+		/// var entity = new Agent
+		/// {
+		/// 	AgentType = someAgentType,
+		/// 	AgentServer = someAgentServer,
+		/// 	RunInterval = 25
+		/// }
+		/// var createdAgent = _agentService.Create(entity);
+		/// </code>
+		/// </example>
 		Agent Create(Agent entity);
 
 		/// <summary>
 		/// Requires the specified Agent.
 		/// <list type="number">
-		/// <item>If <see cref="Artifact.ArtifactID"/> property of <paramref name="entity"/> has positive value, gets entity by id and updates it.</item>
-		/// <item>If <see cref="Agent.AgentType"/> property of <paramref name="entity"/> is existed, gets entity by type and updates it.</item>
-		/// <item>Otherwise creates a new entity using <see cref="ICreateStrategy{T}"/>.</item>
+		/// <item><description>If <see cref="Artifact.ArtifactID"/> property of <paramref name="entity"/> has positive value, gets entity by id and updates it.</description></item>
+		/// <item><description>If <see cref="Agent.AgentType"/> property of <paramref name="entity"/> exists, gets entity by type and updates it.</description></item>
+		/// <item><description>Otherwise creates a new entity using <see cref="ICreateStrategy{T}"/>.</description></item>
 		/// </list>
 		/// </summary>
 		/// <param name="entity">The entity to require.</param>
 		/// <returns>The required entity .</returns>
+		/// <example>
+		/// <code>
+		/// ...
+		/// var agentToUpdate = _agentService.Get(agentArtifactId);
+		/// agentToUpdate.LoggingLevel = AgentLoggingLevelType.Critical;
+		///
+		/// var updatedAgent = _agentService.Require(agentToUpdate); // Finds agent by id, updates LoggingLevel property and returns it
+		/// </code>
+		/// </example>
 		Agent Require(Agent entity);
 
 		/// <summary>
 		/// Deletes the agent by ID.
 		/// </summary>
 		/// <param name="id">The artifact ID of the agent.</param>
+		/// <example>
+		/// <code>
+		/// _agentService.Delete(agentToDeleteArtifactId);
+		/// </code>
+		/// </example>
 		void Delete(int id);
 
 		/// <summary>
 		/// Gets all agent types.
 		/// </summary>
 		/// <returns>The collection of <see cref="AgentType"/> entities.</returns>
+		/// <example>
+		/// <code>
+		/// IEnumerable&#60;AgentType&#62; allAgentTypes = _agentService.GetAllAgentTypes();
+		/// </code>
+		/// </example>
 		IEnumerable<AgentType> GetAllAgentTypes();
 
 		/// <summary>
@@ -45,6 +81,11 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="agentTypeName">The agent type name.</param>
 		/// <returns>The collection of <see cref="AgentServer"/> entities.</returns>
+		/// <example>
+		/// <code>
+		/// IEnumerable&#60;AgentServer&#62; availableAgentServes = _agentService.GetAvailableAgentServers(someAgent.AgentType.Name);
+		/// </code>
+		/// </example>
 		IEnumerable<AgentServer> GetAvailableAgentServers(string agentTypeName);
 
 		/// <summary>
@@ -52,12 +93,22 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="id">The artifact ID of the agent.</param>
 		/// <returns>The <see cref="Agent"/> entity or <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// Agent agent = _agentService.Get(someAgentArtifatId);
+		/// </code>
+		/// </example>
 		Agent Get(int id);
 
 		/// <summary>
 		/// Gets all agents.
 		/// </summary>
 		/// <returns>The collection of <see cref="Agent"/> entities.</returns>
+		/// <example>
+		/// <code>
+		/// IEnumerable&#60;Agent&#62; allAgents = _agentService.GetAllAgents();
+		/// </code>
+		/// </example>
 		IEnumerable<Agent> GetAllAgents();
 
 		/// <summary>
@@ -65,6 +116,11 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="agentTypeName">The agent type name.</param>
 		/// <returns>The collection of <see cref="Agent"/> entities.</returns>
+		/// <example>
+		/// <code>
+		/// IEnumerable&#60;Agent&#62; = _agentService.GetAllAgentsForAgentType("Sample Agent Type Name");
+		/// </code>
+		/// </example>
 		IEnumerable<Agent> GetAllAgentsForAgentType(string agentTypeName);
 
 		/// <summary>
@@ -72,12 +128,24 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="name">The agent type name.</param>
 		/// <returns>The collection of <see cref="AgentType"/> entities.</returns>
+		/// <example>
+		/// <code>
+		/// AgentType sampleAgentType = _agentService.GetAgentType("Sample Agent Type Name");
+		/// </code>
+		/// </example>
 		AgentType GetAgentType(string name);
 
 		/// <summary>
 		/// Updates the specified Agent.
 		/// </summary>
 		/// <param name="entity">The entity to update.</param>
+		/// <example>
+		/// <code>
+		/// var agentToUpdate = _agentService.Get(agentArtifactId);
+		/// agentToUpdate.Enabled = false;
+		/// _agentService.Update(agentToUpdate);
+		/// </code>
+		/// </example>
 		void Update(Agent entity);
 	}
 }

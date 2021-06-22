734578620a39ef1385f8224305cbfe283f95a12a
Klaudia Knafel <klaudia.knafel@relativity.com>
2021-06-22 13:02:08 +0200

    [RTF-1273] Adding examples to IBatchSetService docs.

--- source/Relativity.Testing.Framework.Api/Services/IBatchSetService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IBatchSetService.cs
@@ -6,91 +6,213 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the batch set API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// _batchSetService = relativityFacade.Resolve&lt;IBatchSetService&gt;()
+	/// </code>
+	/// </example>
 	public interface IBatchSetService
 	{
 		/// <summary>
 		/// Creates the specified batch set.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to add the new batch set.</param>
-		/// <param name="entity">The entity to create.</param>
-		/// <returns>The created entity.</returns>
+		/// <param name="entity">The <see cref="BatchSet"/> entity to create.</param>
+		/// <returns>The created <see cref="BatchSet"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// var fieldService =  relativityFacade.Resolve&lt;IFieldService&gt;();
+		/// var workspaceId = 1015427;
+		/// keywordSearch = relativityFacade.Resolve&lt;IKeywordSearchService&gt;().Create(workspaceId, new KeywordSearch());
+		/// batchUnitField = fieldService.Get(workspaceId, "Custodian");
+		/// familyField = fieldService.Get(workspaceId, "Group Identifier");
+		/// reviewedField = fieldService.Get(workspaceId, "Classification Index");
+		/// var batchSet = new BatchSet
+		/// {
+		/// 	Name = Randomizer.GetString(),
+		/// 	BatchSize = 1500,
+		/// 	BatchPrefix = Randomizer.GetString("BS", 3),
+		/// 	DataSource = new NamedArtifact { ArtifactID = keywordSearch.ArtifactID},
+		/// 	BatchUnitField = new NamedArtifact { ArtifactID = batchUnitField.ArtifactID},
+		/// 	FamilyField = new NamedArtifact { ArtifactID = familyField.ArtifactID },
+		/// 	ReviewedField = new NamedArtifact { ArtifactID = reviewedField.ArtifactID },
+		/// 	AutoBatchSettings = new AutoBatchSettings
+		/// 	{
+		/// 		AutoCreateRateInMinutes = 10,
+		/// 		MinimumBatchSize = 10
+		/// 	}
+		/// };
+		/// BatchSet batchSet = _batchSetService.Create(workspaceId, batchSet);
+		/// </code>
+		/// </example>
 		BatchSet Create(int workspaceId, BatchSet entity);
 
 		/// <summary>
 		/// Creates the specified batch set.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to add the new batch set.</param>
-		/// <param name="entity">The entity to create.</param>
+		/// <param name="entity">The <see cref="BatchSet"/> entity to create.</param>
 		/// <param name="userCredentials">User credentials to be used when perfroming action over Relativity Api.</param>
-		/// <returns>The created entity.</returns>
+		/// <returns>The created <see cref="BatchSet"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// var fieldService =  relativityFacade.Resolve&lt;IFieldService&gt;();
+		/// var workspaceId = 1015427;
+		/// keywordSearch = relativityFacade.Resolve&lt;IKeywordSearchService&gt;().Create(workspaceId, new KeywordSearch());
+		/// batchUnitField = fieldService.Get(workspaceId, "Custodian");
+		/// familyField = fieldService.Get(workspaceId, "Group Identifier");
+		/// reviewedField = fieldService.Get(workspaceId, "Classification Index");
+		/// var batchSet = new BatchSet
+		/// {
+		/// 	Name = Randomizer.GetString(),
+		/// 	BatchSize = 1500,
+		/// 	BatchPrefix = Randomizer.GetString("BS", 3),
+		/// 	DataSource = new NamedArtifact { ArtifactID = keywordSearch.ArtifactID},
+		/// 	BatchUnitField = new NamedArtifact { ArtifactID = batchUnitField.ArtifactID},
+		/// 	FamilyField = new NamedArtifact { ArtifactID = familyField.ArtifactID },
+		/// 	ReviewedField = new NamedArtifact { ArtifactID = reviewedField.ArtifactID },
+		/// 	AutoBatchSettings = new AutoBatchSettings
+		/// 	{
+		/// 		AutoCreateRateInMinutes = 10,
+		/// 		MinimumBatchSize = 10
+		/// 	}
+		/// };
+		/// var userCredentials = new UserCredentials
+		/// {
+		/// 	Username = "SomeUsername",
+		/// 	Password = "SomePassword"
+		/// };
+		/// BatchSet batchSet = _batchSetService.Create(workspaceId, batchSet, userCredentials);
+		/// </code>
+		/// </example>
 		BatchSet Create(int workspaceId, BatchSet entity, UserCredentials userCredentials);
 
 		/// <summary>
-		/// Gets the batch set by the specified ID.
+		/// Gets the <see cref="BatchSet"/> by the specified ID.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to get the batch set.</param>
 		/// <param name="entityId">The Artifact ID of the batch set.</param>
 		/// <returns>>The <see cref="Production"/> entity or <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// BatchSet batchSet = _batchSetService.Get(1015427, batchSetArtifactId);
+		/// </code>
+		/// </example>
 		BatchSet Get(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Gets the batch set by the specified ID.
+		/// Gets the <see cref="BatchSet"/> by the specified ID.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to get the batch set.</param>
 		/// <param name="entityId">The Artifact ID of the batch set.</param>
 		/// <param name="userCredentials">User credentials to be used when perfroming action over Relativity Api.</param>
 		/// <returns>>The <see cref="Production"/> entity or <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// var userCredentials = new UserCredentials
+		/// {
+		/// 	Username = "SomeUsername",
+		/// 	Password = "SomePassword"
+		/// };
+		/// BatchSet batchSet = _batchSetService.Get(1015427, batchSetArtifactId, userCredentials);
+		/// </code>
+		/// </example>
 		BatchSet Get(int workspaceId, int entityId, UserCredentials userCredentials);
 
 		/// <summary>
-		/// Determines whether the batch set with the specified case artifact ID exists.
+		/// Determines whether the <see cref="BatchSet"/> with the specified case artifact ID exists.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace.</param>
 		/// <param name="entityId">The Artifact ID of the batch set.</param>
 		/// <returns><see langword="true"/> if a batch set exists; otherwise, <see langword="false"/>.</returns>
+		/// <example>
+		/// <code>
+		/// var batchSetExists = _batchSetService.Exists(1015427, batchSetArtifactId);
+		/// </code>
+		/// </example>
 		bool Exists(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Determines whether the batch set with the specified case artifact ID exists.
+		/// Determines whether the <see cref="BatchSet"/> with the specified case artifact ID exists.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace.</param>
 		/// <param name="entityId">The Artifact ID of the batch set.</param>
 		/// <param name="userCredentials">User credentials to be used when perfroming action over Relativity Api.</param>
 		/// <returns><see langword="true"/> if a batch set exists; otherwise, <see langword="false"/>.</returns>
+		/// <example>
+		/// <code>
+		/// var userCredentials = new UserCredentials
+		/// {
+		/// 	Username = "SomeUsername",
+		/// 	Password = "SomePassword"
+		/// };
+		/// BatchSet batchSet = _batchSetService.Exists(1015427, batchSetArtifactId, userCredentials);
+		/// </code>
+		/// </example>
 		bool Exists(int workspaceId, int entityId, UserCredentials userCredentials);
 
 		/// <summary>
-		/// Runs create batches operation for a specific batch set.
+		/// Runs create batches operation for a specific <see cref="BatchSet"/>.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of a workspace.</param>
 		/// <param name="entityId">The Artifact ID of a batch set.</param>
 		/// <returns>>The <see cref="BatchProcessResult"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// BatchProcessResult result = _batchSetService.CreateBatches(1015427, batchSetArtifactId);
+		/// </code>
+		/// </example>
 		BatchProcessResult CreateBatches(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Runs create batches operation for a specific batch set.
+		/// Runs create batches operation for a specific <see cref="BatchSet"/>.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of a workspace.</param>
 		/// <param name="entityId">The Artifact ID of a batch set.</param>
 		/// <param name="userCredentials">User credentials to be used when perfroming action over Relativity Api.</param>
 		/// <returns>>The <see cref="BatchProcessResult"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// var userCredentials = new UserCredentials
+		/// {
+		/// 	Username = "SomeUsername",
+		/// 	Password = "SomePassword"
+		/// };
+		/// BatchProcessResult result = _batchSetService.CreateBatches(1015427, batchSetArtifactId, userCredentials);
+		/// </code>
+		/// </example>
 		BatchProcessResult CreateBatches(int workspaceId, int entityId, UserCredentials userCredentials);
 
 		/// <summary>
-		/// Runs purge bathces operation for a specific batch set.
+		/// Runs purge batches operation for a specific <see cref="BatchSet"/>.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of a workspace.</param>
 		/// <param name="entityId">The Artifact ID of a batch set.</param>
 		/// <returns>>The <see cref="BatchProcessResult"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// BatchProcessResult result = _batchSetService.PurgeBatches(1015427, batchSetArtifactId);
+		/// </code>
+		/// </example>
 		BatchProcessResult PurgeBatches(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Runs purge bathces operation for a specific batch set.
+		/// Runs purge bathces operation for a specific <see cref="BatchSet"/>.
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of a workspace.</param>
 		/// <param name="entityId">The Artifact ID of a batch set.</param>
 		/// <param name="userCredentials">User credentials to be used when perfroming action over Relativity Api.</param>
 		/// <returns>>The <see cref="BatchProcessResult"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// var userCredentials = new UserCredentials
+		/// {
+		/// 	Username = "SomeUsername",
+		/// 	Password = "SomePassword"
+		/// };
+		/// BatchProcessResult result = _batchSetService.PurgeBatches(1015427, batchSetArtifactId, userCredentials);
+		/// </code>
+		/// </example>
 		BatchProcessResult PurgeBatches(int workspaceId, int entityId, UserCredentials userCredentials);
 	}
 }

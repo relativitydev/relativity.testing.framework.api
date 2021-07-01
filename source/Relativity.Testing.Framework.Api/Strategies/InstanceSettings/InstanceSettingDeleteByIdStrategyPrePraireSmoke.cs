1d8b920cc6d489543650c324d511f7995bb6d80b
Maria Kizhaeva <maria.kizhaeva@relativity.com>
2021-07-01 13:02:59 +0200

    [RTF-1331] V1 Strategy for InstanceSetting Delete operation

---
+++ source/Relativity.Testing.Framework.Api/Strategies/InstanceSettings/InstanceSettingDeleteByIdStrategyPrePraireSmoke.cs
@@ -0,0 +1,23 @@
+﻿using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Models;
+using Relativity.Testing.Framework.Strategies;
+using Relativity.Testing.Framework.Versioning;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	[VersionRange("<12.1")]
+	internal class InstanceSettingDeleteByIdStrategyPrePraireSmoke : DeleteByIdStrategy<InstanceSetting>
+	{
+		private readonly IRestService _restService;
+
+		public InstanceSettingDeleteByIdStrategyPrePraireSmoke(IRestService restService)
+		{
+			_restService = restService;
+		}
+
+		protected override void DoDelete(int id)
+		{
+			_restService.Delete($"Relativity.InstanceSettings/workspace/-1/instancesettings/{id}");
+		}
+	}
+}
---
+++ source/Relativity.Testing.Framework.Api/Strategies/InstanceSettings/InstanceSettingDeleteByIdStrategyV1.cs
@@ -0,0 +1,23 @@
+﻿using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Models;
+using Relativity.Testing.Framework.Strategies;
+using Relativity.Testing.Framework.Versioning;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	[VersionRange(">=12.1")]
+	internal class InstanceSettingDeleteByIdStrategyV1 : DeleteByIdStrategy<InstanceSetting>
+	{
+		private readonly IRestService _restService;
+
+		public InstanceSettingDeleteByIdStrategyV1(IRestService restService)
+		{
+			_restService = restService;
+		}
+
+		protected override void DoDelete(int id)
+		{
+			_restService.Delete($"/Relativity.REST/api/relativity-environment/V1/workspaces/-1/instance-settings/{id}");
+		}
+	}
+}

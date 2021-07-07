4f5972373668c06dec5f3463eadf7ed3b76cee3b
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-07-07 15:54:46 +0200

    RTF-1254 - Create strategies for DocumentStatusManager

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.27.0] - 2021-07-30
+
+### Added
+
+- Get Imaging Document Status methods for IImagingDocumentService. - [RTF-1254](https://jira.kcura.com/browse/RTF-1254)
+
 ## [3.26.0] - 2021-07-29
 
 ### Added
---
+++ source/Relativity.Testing.Framework.Api/Services/Imaging/IImagingDocumentService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Services
+{
+	internal interface IImagingDocumentService
+	{
+		/// <summary>
+		/// Get status information about the imaging job for a document.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="documentArtifactId">The Document ArtifactID.</param>
+		/// <returns>Return a <see cref="DocumentStatus"/> of imaging job for a document.</returns>
+		DocumentStatus GetStatus(int workspaceId, int documentArtifactId);
+
+		/// <summary>
+		/// Get status information about the imaging job for a document.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="documentArtifactId">The Document ArtifactID.</param>
+		/// <returns>A task with <see cref="DocumentStatus"/>.</returns>
+		Task<DocumentStatus> GetStatusAsync(int workspaceId, int documentArtifactId);
+	}
+}

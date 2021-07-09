82022d1379a09c090eae99991d92dc10a3db3c61
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-07-09 10:03:20 +0200

    RTF-1249 - add Imaging service

---
+++ source/Relativity.Testing.Framework.Api/Services/IImagingService.cs
@@ -0,0 +1,97 @@
+﻿using System.Threading.Tasks;
+using Relativity.Testing.Framework.Api.Strategies;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Services
+{
+	/// <summary>
+	/// Represents the Imaging API service.
+	/// </summary>
+	/// <example>
+	/// <code>
+	/// _imagingService = relativityFacade.Resolve&lt;IImagingService&gt;();
+	/// </code>
+	/// </example>
+	public interface IImagingService
+	{
+		/// <summary>
+		/// Creates a new Imaging Profile instance, specifying only Basic Profile options. Native Profile options will be set to
+		/// default values, as occurs when creating a Basic Profile in the UI.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="dto">A <see cref="CreateBasicImagingProfileDTO"/> object corresponding to the desired Imaging Profile object.</param>
+		/// <returns>Returns an <see cref="ImagingProfile"/> instance.</returns>
+		ImagingProfile CreateBasic(int workspaceId, CreateBasicImagingProfileDTO dto);
+
+		/// <summary>
+		/// Creates a new Imaging Profile instance, specifying only Basic Profile options. Native Profile options will be set to
+		/// default values, as occurs when creating a Basic Profile in the UI.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="dto">A <see cref="CreateBasicImagingProfileDTO"/> object corresponding to the desired Imaging Profile object.</param>
+		/// <returns>A <see cref="Task"/> with an <see cref="ImagingProfile"/> instance.</returns>
+		Task<ImagingProfile> CreateBasicAsync(int workspaceId, CreateBasicImagingProfileDTO dto);
+
+		/// <summary>
+		/// Creates a new Imaging Profile instance, specifying both Basic and Native Profile options.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="dto">A <see cref="CreateNativeImagingProfileDTO"/> object corresponding to the desired Imaging Profile object.</param>
+		/// <returns>Returns an <see cref="ImagingProfile"/> instance.</returns>
+		ImagingProfile CreateNative(int workspaceId, CreateNativeImagingProfileDTO dto);
+
+		/// <summary>
+		/// Creates a new Imaging Profile instance, specifying both Basic and Native Profile options.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="dto">A <see cref="CreateNativeImagingProfileDTO"/> object corresponding to the desired Imaging Profile object.</param>
+		/// <returns>A <see cref="Task"/> with an <see cref="ImagingProfile"/> instance.</returns>
+		Task<ImagingProfile> CreateNativeAsync(int workspaceId, CreateNativeImagingProfileDTO dto);
+
+		/// <summary>
+		/// Updates an existing Imaging Profile instance.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="imagingProfile">An <see cref="ImagingProfile"/> object with updated fields, reflecting the desired final state of the Imaging Profile.</param>
+		void Update(int workspaceId, ImagingProfile imagingProfile);
+
+		/// <summary>
+		/// Updates an existing Imaging Profile instance.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="imagingProfile">An <see cref="ImagingProfile"/> object with updated fields, reflecting the desired final state of the Imaging Profile.</param>
+		/// <returns>A <see cref="Task"/> with an <see cref="ImagingProfile"/> instance.</returns>
+		Task UpdateAsync(int workspaceId, ImagingProfile imagingProfile);
+
+		/// <summary>
+		/// Retrieves the <see cref="ImagingProfile"/> with the specified Artifact ID.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="imagingProfileId">The Artifact ID of the <see cref="ImagingProfile"/> instance to read.</param>
+		/// <returns>Returns an <see cref="ImagingProfile"/> instance.</returns>
+		ImagingProfile Get(int workspaceId, int imagingProfileId);
+
+		/// <summary>
+		/// Retrieves the <see cref="ImagingProfile"/> with the specified Artifact ID.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="imagingProfileId">The Artifact ID of the <see cref="ImagingProfile"/> instance to read.</param>
+		/// <returns>A <see cref="Task"/> with an <see cref="ImagingProfile"/> instance.</returns>
+		Task<ImagingProfile> GetAsync(int workspaceId, int imagingProfileId);
+
+		/// <summary>
+		/// Deletes an Imaging Profile instance.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="imagingProfileId">The Artifact ID of the <see cref="ImagingProfile"/> instance to delete.</param>
+		void Delete(int workspaceId, int imagingProfileId);
+
+		/// <summary>
+		/// Deletes an Imaging Profile instance.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <param name="imagingProfileId">The Artifact ID of the <see cref="ImagingProfile"/> instance to delete.</param>
+		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		Task DeleteAsync(int workspaceId, int imagingProfileId);
+	}
+}
---
+++ source/Relativity.Testing.Framework.Api/Services/ImagingService.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Relativity.Testing.Framework.Api.Strategies;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Services
+{
+	internal class ImagingService : IImagingService
+	{
+		private readonly IBasicImagingProfileCreateStrategy _basicImagingProfileCreateStrategy;
+		private readonly INativeImagingProfileCreateStrategy _nativeImagingProfileCreateStrategy;
+		private readonly IImagingProfileUpdateStrategy _imagingProfileUpdateStrategy;
+		private readonly IImagingProfileGetStrategy _imagingProfileGetStrategy;
+		private readonly IImagingProfileDeleteStrategy _imagingProfileDeleteStrategy;
+
+		public ImagingService(
+						IBasicImagingProfileCreateStrategy basicImagingProfileCreateStrategy,
+						INativeImagingProfileCreateStrategy nativeImagingProfileCreateStrategy,
+						IImagingProfileUpdateStrategy imagingProfileUpdateStrategy,
+						IImagingProfileGetStrategy imagingProfileGetStrategy,
+						IImagingProfileDeleteStrategy imagingProfileDeleteStrategy)
+		{
+			_basicImagingProfileCreateStrategy = basicImagingProfileCreateStrategy;
+			_nativeImagingProfileCreateStrategy = nativeImagingProfileCreateStrategy;
+			_imagingProfileUpdateStrategy = imagingProfileUpdateStrategy;
+			_imagingProfileGetStrategy = imagingProfileGetStrategy;
+			_imagingProfileDeleteStrategy = imagingProfileDeleteStrategy;
+		}
+
+		public ImagingProfile CreateBasic(int workspaceId, CreateBasicImagingProfileDTO dto)
+			=> _basicImagingProfileCreateStrategy.Create(workspaceId, dto);
+
+		public async Task<ImagingProfile> CreateBasicAsync(int workspaceId, CreateBasicImagingProfileDTO dto)
+			=> await _basicImagingProfileCreateStrategy.CreateAsync(workspaceId, dto).ConfigureAwait(false);
+
+		public ImagingProfile CreateNative(int workspaceId, CreateNativeImagingProfileDTO dto)
+			=> _nativeImagingProfileCreateStrategy.Create(workspaceId, dto);
+
+		public async Task<ImagingProfile> CreateNativeAsync(int workspaceId, CreateNativeImagingProfileDTO dto)
+			=> await _nativeImagingProfileCreateStrategy.CreateAsync(workspaceId, dto).ConfigureAwait(false);
+
+		public void Update(int workspaceId, ImagingProfile imagingProfile)
+			=> _imagingProfileUpdateStrategy.Update(workspaceId, imagingProfile);
+
+		public async Task UpdateAsync(int workspaceId, ImagingProfile imagingProfile)
+			=> await _imagingProfileUpdateStrategy.UpdateAsync(workspaceId, imagingProfile).ConfigureAwait(false);
+
+		public ImagingProfile Get(int workspaceId, int imagingProfileId)
+			=> _imagingProfileGetStrategy.Get(workspaceId, imagingProfileId);
+
+		public async Task<ImagingProfile> GetAsync(int workspaceId, int imagingProfileId)
+			=> await _imagingProfileGetStrategy.GetAsync(workspaceId, imagingProfileId).ConfigureAwait(false);
+
+		public void Delete(int workspaceId, int imagingProfileId)
+			=> _imagingProfileDeleteStrategy.Delete(workspaceId, imagingProfileId);
+
+		public async Task DeleteAsync(int workspaceId, int imagingProfileId)
+			=> await _imagingProfileDeleteStrategy.DeleteAsync(workspaceId, imagingProfileId).ConfigureAwait(false);
+	}
+}

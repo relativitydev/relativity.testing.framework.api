ea23cc79149bee56d12de60cb673203d055d4f46
maria-kizhaeva <82820579+maria-kizhaeva@users.noreply.github.com>
2021-07-20 20:31:06 +0200

    [RTF-1287] examples for IHttpService (#81)
    [RTF-1282] examples for IHttpService

--- source/Relativity.Testing.Framework.Api/Services/IHttpService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IHttpService.cs
@@ -7,6 +7,11 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the HTTP service that provides a set of methods to communicate with Relativity REST API.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// _httpService = relativityFacade.Resolve&lt;IHttpService&gt;();
+	/// </code>
+	/// </example>
 	public interface IHttpService
 	{
 		/// <summary>
@@ -30,6 +35,11 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// JObject result = _restService.Get&lt;JObject&gt;($"relativity.agents/workspace/-1/agents/{id}");
+		/// </code>
+		/// </example>
 		TResult Get<TResult>(string relativeUri, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -43,6 +53,12 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// string url = "some/url"
+		/// ImagingSet result = await _restService.GetAsync&lt;ImagingSet&gt;(url).ConfigureAwait(false);
+		/// </code>
+		/// </example>
 		Task<TResult> GetAsync<TResult>(string relativeUri, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -59,6 +75,37 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// var dto = new
+		/// {
+		/// 	Request = new
+		/// 	{
+		/// 		Object = new
+		/// 		{
+		/// 			ArtifactID = 12345
+		/// 		}
+		/// 	}
+		/// };
+		///
+		/// _restService.Post&lt;string&gt;($"Relativity.Objects/workspace/{workspaceId}/object/delete", dto);
+		/// </code>
+		/// </example>
+		/// <example>
+		/// <code>
+		/// var dto = new
+		/// {
+		/// 	batchSet = new
+		/// 	{
+		/// 		Name = "Test Batch Set",
+		/// 		BatchPrefix = "TEST1",
+		/// 		BatchSize = 10,
+		/// 	}
+		/// };
+		///
+		/// BatchSet result =  _restService.Post&lt;BatchSet&gt;($"Relativity.Services.Review.Batching.IBatchingModule/workspaces/{workspaceId}/batching/CreateBatchSet", dto, new UserCredentials { Username = "FakeAccount", Password = "FakePassword" });
+		/// </code>
+		/// </example>
 		TResult Post<TResult>(string relativeUri, object content = null, double timeout = 2, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -71,6 +118,17 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// var dto = new
+		/// {
+		/// 	workspaceArtifactID = 54321,
+		/// 	searchArtifactID = 12345
+		/// };
+		///
+		/// _restService.Post("Relativity.Services.Search.ISearchModule/Keyword%20Search%20Manager/DeleteSingleAsync", dto);
+		/// </code>
+		/// </example>
 		void Post(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -87,6 +145,23 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// string url = "some/url"
+		/// var request = new
+		/// {
+		/// 	request = new
+		/// 	{
+		/// 		dto = new
+		/// 		{
+		/// 			ArtifactID = 12345
+		/// 		}
+		/// 	}
+		/// };
+		///
+		/// var applicationFieldCodeId = await _restService.PostAsync&lt;int&gt;(url, request).ConfigureAwait(false);
+		/// </code>
+		/// </example>
 		Task<TResult> PostAsync<TResult>(string relativeUri, object content = null, double timeout = 2, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -100,6 +175,23 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
 		/// <returns>>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		/// <example>
+		/// <code>
+		/// string url = "some/url"
+		/// var request = new
+		/// {
+		/// 	request = new
+		/// 	{
+		/// 		dto = new
+		/// 		{
+		/// 			ArtifactID = 12345
+		/// 		}
+		/// 	}
+		/// };
+		///
+		/// await _restService.PostAsync(url, request).ConfigureAwait(false);
+		/// </code>
+		/// </example>
 		Task PostAsync(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -115,6 +207,23 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// LibraryApplicationInstallStatusResponse response;
+		/// using (var form = new MultipartFormDataContent())
+		/// using (var memory = new StreamContent(new MemoryStream(bytes)))
+		/// {
+		/// 	var optionsString = JsonConvert.SerializeObject(dto, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+		/// 	using (var optionsContent = new StringContent(optionsString, Encoding.UTF8, "application/json"))
+		/// 	{
+		/// 		form.Add(optionsContent, "request");
+		/// 		form.Add(memory, "rapStream");
+		///
+		/// 		response = _restService.Put&lt;LibraryApplicationInstallStatusResponse&gt;("Relativity.LibraryApplications/workspace/-1/libraryapplications", form);
+		/// 	}
+		/// }
+		/// </code>
+		/// </example>
 		TResult Put<TResult>(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -127,6 +236,27 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// int artifactId = 123453;
+		/// var dto = new
+		/// {
+		/// 	groupRequest = new
+		/// 	{
+		/// 		Client = new
+		/// 		{
+		/// 			Secured = false,
+		/// 			Value = new
+		/// 			{
+		/// 				12345
+		/// 			}
+		/// 		},
+		/// 	}
+		/// };
+		///
+		/// _restService.Put($"relativity.groups/workspace/-1/groups/artifactId", dto);
+		/// </code>
+		/// </example>
 		void Put(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -142,6 +272,23 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// LibraryApplicationInstallStatusResponse response;
+		/// using (var form = new MultipartFormDataContent())
+		/// using (var memory = new StreamContent(new MemoryStream(bytes)))
+		/// {
+		/// 	var optionsString = JsonConvert.SerializeObject(dto, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+		/// 	using (var optionsContent = new StringContent(optionsString, Encoding.UTF8, "application/json"))
+		/// 	{
+		/// 		form.Add(optionsContent, "request");
+		/// 		form.Add(memory, "rapStream");
+		///
+		/// 		response = await _restService.Put&lt;LibraryApplicationInstallStatusResponse&gt;("Relativity.LibraryApplications/workspace/-1/libraryapplications", form).ConfigureAwait(false);
+		/// 	}
+		/// }
+		/// </code>
+		/// </example>
 		Task<TResult> PutAsync<TResult>(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -155,6 +302,27 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
 		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		/// <example>
+		/// <code>
+		/// int artifactId = 123453;
+		/// var dto = new
+		/// {
+		/// 	groupRequest = new
+		/// 	{
+		/// 		Client = new
+		/// 		{
+		/// 			Secured = false,
+		/// 			Value = new
+		/// 			{
+		/// 				12345
+		/// 			}
+		/// 		},
+		/// 	}
+		/// };
+		///
+		/// await _restService.Put($"relativity.groups/workspace/-1/groups/artifactId", dto).ConfigureAwait(false);
+		/// </code>
+		/// </example>
 		Task PutAsync(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -170,6 +338,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// int workspaceId = 12345;
+		/// int applicationFieldCodeId = 345;
+		/// string url = $"relativity-imaging/v1/workspaces/{workspaceId}/application-field-codes/{applicationFieldCodeId}";
+		/// _restService.Delete&lt;bool&gt;(url);
+		/// </code>
+		/// </example>
 		TResult Delete<TResult>(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -182,6 +358,17 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// int agentId = 1234;
+		/// var dto = new
+		/// {
+		/// 	force = true
+		/// };
+		///
+		/// _restService.Delete($"relativity.agents/workspace/-1/agents/{agentId}", dto);
+		/// </code>
+		/// </example>
 		void Delete(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -197,6 +384,12 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <exception cref="HttpRequestException">
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
+		/// <example>
+		/// <code>
+		/// string url = "some/url";
+		/// await _restService.DeleteAsync&lt;bool&gt;(url).ConfigureAwait(false);
+		/// </code>
+		/// </example>
 		Task<TResult> DeleteAsync<TResult>(string relativeUri, object content = null, UserCredentials userCredentials = null);
 
 		/// <summary>
@@ -210,6 +403,12 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// The response has <see cref="HttpResponseMessage.IsSuccessStatusCode"/> equal to <see langword="false"/>.
 		/// </exception>
 		/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+		/// <example>
+		/// <code>
+		/// string url = "some/url";
+		/// await _restService.DeleteAsync(url).ConfigureAwait(false);
+		/// </code>
+		/// </example>
 		Task DeleteAsync(string relativeUri, object content = null, UserCredentials userCredentials = null);
 	}
 }

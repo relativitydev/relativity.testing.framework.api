a043084d4a9016348f498b576afa589b9958ce50
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-07-30 11:37:33 +0200

    RTF-1255 - Create strategies for ImagingEnvironmentManager

---
+++ source/Relativity.Testing.Framework.Api/Strategies/Imaging/Environment/IRemoveInactiveImagingJobsStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	internal interface IRemoveInactiveImagingJobsStrategy
+	{
+		void Remove();
+
+		Task RemoveAsync();
+	}
+}
---
+++ source/Relativity.Testing.Framework.Api/Strategies/Imaging/Environment/RemoveInactiveImagingJobsStrategyNotSupported.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Relativity.Testing.Framework.Versioning;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	[VersionRange("<12.1")]
+	internal class RemoveInactiveImagingJobsStrategyNotSupported : IRemoveInactiveImagingJobsStrategy
+	{
+		public void Remove()
+		{
+			throw new ArgumentException("The method Remove Inactive Imaging Jobs does not support version of Relativity lower than 12.1.");
+		}
+
+		public Task RemoveAsync()
+		{
+			throw new ArgumentException("The method Remove Inactive Imaging Jobs does not support version of Relativity lower than 12.1.");
+		}
+	}
+}

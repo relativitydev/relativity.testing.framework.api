9732bb0b7e92164b279babe3d932e7dd0237a610
jonatanjakoniuk <81292620+jonatanjakoniuk@users.noreply.github.com>
2021-07-30 09:17:23 +0200

    RTF-1254 - Create strategies for DocumentStatusManager (#96)

---
+++ source/Relativity.Testing.Framework.Api/Services/Imaging/ImagingDocumentService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Relativity.Testing.Framework.Api.Strategies;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Services
+{
+	internal class ImagingDocumentService : IImagingDocumentService
+	{
+		private readonly IImagingDocumentStatusGetByIdStrategy _imagingDocumentStatusGetByIdStrategy;
+
+		public ImagingDocumentService(IImagingDocumentStatusGetByIdStrategy imagingDocumentStatusGetByIdStrategy)
+		{
+			_imagingDocumentStatusGetByIdStrategy = imagingDocumentStatusGetByIdStrategy;
+		}
+
+		public DocumentStatus GetStatus(int workspaceId, int documentArtifactId)
+			=> _imagingDocumentStatusGetByIdStrategy.Get(workspaceId, documentArtifactId);
+
+		public async Task<DocumentStatus> GetStatusAsync(int workspaceId, int documentArtifactId)
+			=> await _imagingDocumentStatusGetByIdStrategy.GetAsync(workspaceId, documentArtifactId).ConfigureAwait(false);
+	}
+}

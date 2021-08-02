18bb5c0b8a7ebf347d768e55f0307a32b4ce1d9f
Klaudia Knafel <klaudia.knafel@relativity.com>
2021-08-02 19:26:52 +0200

    [RTF-1287] Adding examples to ILayoutService, fixing return type in GetCategories method.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.1.0] - 2021-08-02
+
+### Fix
+
+- Return type of GetCategories method in ILayoutService. - [RTF-1287](https://jira.kcura.com/browse/RTF-1287)
+
 ## [4.0.0] - 2021-07-30
 
 ### Changed
--- source/Relativity.Testing.Framework.Api/Services/ILayoutService.cs
+++ source/Relativity.Testing.Framework.Api/Services/ILayoutService.cs
@@ -7,6 +7,11 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the layout API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// _layoutService = relatvityFacade.Resolve&lt;ILayoutService&gt;();
+	/// </code>
+	/// </example>
 	public interface ILayoutService
 	{
 		/// <summary>
@@ -16,20 +21,63 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entity">The entity to create.</param>
 		/// <returns>The created entity.</returns>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existingObjectTypeId = 1;
+		/// ObjectType objectType =  relatvityFacade.Resolve&lt;IObjectTypeService&gt;()
+		/// 	.Resolve(workspaceId, existingObjectTypeId);
+		/// var entity = new Layout
+		/// {
+		/// 	Name = "Sample Layout Name",
+		/// 	ObjectType = objectType
+		/// };
+		/// Layout createdLayout = _layoutService.Create(workspaceId, entity);
+		/// </code>
+		/// </example>
 		Layout Create(int workspaceId, Layout entity);
 
 		/// <summary>
 		/// Requires the specified layout.
 		/// <list type="number">
-		/// <item>If <see cref="Artifact.ArtifactID"/> property of <paramref name="entity"/> has positive value, gets entity by ID and updates it.</item>
-		/// <item>If <see cref="NamedArtifact.Name"/> property of <paramref name="entity"/> have a value, gets entity by name and updates it if it exists.</item>
-		/// <item>Otherwise creates a new entity using <see cref="ICreateWorkspaceEntityStrategy{T}"/>.</item>
+		/// <item><description>If <see cref="Artifact.ArtifactID"/> property of <paramref name="entity"/> has positive value, gets entity by ID and updates it.</description></item>
+		/// <item><description>If <see cref="NamedArtifact.Name"/> property of <paramref name="entity"/> have a value, gets entity by name and updates it if it exists.</description></item>
+		/// <item><description>Otherwise creates a new entity using <see cref="ICreateWorkspaceEntityStrategy{T}"/>.</description></item>
 		/// </list>
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to require layout,
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entity">The entity to require.</param>
 		/// <returns>The entity required.</returns>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existingObjectTypeId = 1;
+		/// var existigLayoutId = 2;
+		/// ObjectType objectType =  relatvityFacade.Resolve&lt;IObjectTypeService&gt;()
+		/// 	.Resolve(workspaceId, existingObjectTypeId);
+		/// var entityToUpdateById = new Layout
+		/// {
+		/// 	Name = "Sample Updated Layout Name",
+		/// 	ObjectType = objectType,
+		/// 	ArtifactID = existigLayoutId
+		/// };
+		/// Layout layoutUpdatedById = _layoutService.Require(workspaceId, entityToUpdateById);
+		/// var entityToUpdateByName = new Layout
+		/// {
+		/// 	Name = "Sample Existing Layout Name",
+		/// 	ObjectType = objectType,
+		/// 	AllowCopyFromPrevious = true
+		/// };
+		/// Layout layoutUpdatedByName = _layoutService.Require(workspaceId, entityToUpdateByName);
+		/// var entityToCreate = new Layout
+		/// {
+		/// 	Name = "Sample Not Existing Layout Name",
+		/// 	ObjectType = objectType,
+		/// };
+		/// Layout createdLayout = _layoutService.Require(workspaceId, entityToCreate);
+		/// </code>
+		/// </example>
 		Layout Require(int workspaceId, Layout entity);
 
 		/// <summary>
@@ -38,6 +86,13 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to delete the layout,
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entityId">The artifact ID of the layout.</param>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existigLayoutId = 1;
+		/// _layoutService.Delete(workspaceId, existigLayoutId);
+		/// </code>
+		/// </example>
 		void Delete(int workspaceId, int entityId);
 
 		/// <summary>
@@ -46,7 +101,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to get the layout,
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entityId">The artifact ID of the layout.</param>
-		/// <returns>>The <see cref="Layout"/> entity.</returns>
+		/// <returns>The <see cref="Layout"/> entity.</returns>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existigLayoutId = 1;
+		/// Layout layout = _layoutService.Get(workspaceId, existigLayoutId);
+		/// </code>
+		/// </example>
 		Layout Get(int workspaceId, int entityId);
 
 		/// <summary>
@@ -54,6 +116,12 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="workspaceId">The workspace ID.</param>
 		/// <returns>The list eligible to be a layout owner.</returns>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// List&lt;NamedArtifact&gt; eligibleOwners = _layoutService.GetEligibleOwners(workspaceId);
+		/// </code>
+		/// </example>
 		List<NamedArtifact> GetEligibleOwners(int workspaceId);
 
 		/// <summary>
@@ -62,6 +130,15 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to update the layout,
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entity">The entity to update.</param>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existingLayoutId = 1;
+		/// Layout layoutToUpdate = _layoutService.Get(workspaceId, existingLayoutId);
+		/// layoutToUpdate.Name = "Some Updated Layout Name";
+		/// _layoutService.Update(workspaceId, layoutToUpdate);
+		/// </code>
+		/// </example>
 		void Update(int workspaceId, Layout entity);
 
 		/// <summary>
@@ -71,6 +148,27 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entity">The layout to build. If ArtifactId is provided, it will be used, otherwise it will be looked up by name. If neither are provided, this will put us in an exceptional state.</param>
 		/// <param name="categoryFields">The fields to add to a category.</param>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existingObjectTypeId = 1;
+		/// var existingFieldId = 2;
+		/// var existingFieldTypeId = 3;
+		/// Layout layout = _layoutService.Get(workspaceId, existingLayoutId);
+		/// var fieldsToAdd = new List&lt;CategoryField&gt;
+		/// {
+		/// 	new CategoryField
+		/// 	{
+		/// 		DisplayName = "Some Field Name",
+		/// 		FieldArtifactID = existingFieldId,
+		/// 		FieldDisplayType = FieldDisplayType.Decimal,
+		/// 		FieldTypeID = existingFieldTypeId,
+		/// 		IsRequired = true
+		/// 	}
+		/// };
+		/// _layoutService.AddFields(workspaceId, layout, fieldsToAdd);
+		/// </code>
+		/// </example>
 		void AddFields(int workspaceId, Layout entity, List<CategoryField> categoryFields);
 
 		/// <summary>
@@ -79,6 +177,15 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to update the layout,
 		/// or use -1 to indicate the admin-level context.</param>
 		/// <param name="entity">The layout to get the categories for. If ArtifactId is provided, it will be used, otherwise it will be looked up by name. If neither are provided, this will put us in an exceptional state.</param>
-		void GetCategories(int workspaceId, Layout entity);
+		/// <returns>The list of categories in layout.</returns>
+		/// <example>
+		/// <code>
+		/// var workspaceId = 1015427;
+		/// var existingLayoutId = 1;
+		/// Layout layout = _layoutService.Get(workspaceId, existingLayoutId);
+		/// List&lt;Category&gt; categories = _layoutService.GetCategories(workspaceId, layout);
+		/// </code>
+		/// </example>
+		List<Category> GetCategories(int workspaceId, Layout entity);
 	}
 }

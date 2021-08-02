87c2d38113ce641461161bb3bac98447ae351133
Paul Letang <71905764+paulletang@users.noreply.github.com>
2021-08-02 13:53:33 -0500

    [TESTENG-1281] Adding Async methods for ObjectManagement/Query code. (#94)
    * [TESTENG-1281] Adding async methods for object manager/query code, and updating the documentation for both sets of methods.

---
+++ source/Relativity.Testing.Framework.Api/ObjectManagement/ObjectQueryAsync`1.cs
@@ -0,0 +1,43 @@
+﻿using Relativity.Testing.Framework.Api.Models;
+using Relativity.Testing.Framework.Api.Querying;
+
+namespace Relativity.Testing.Framework.Api.ObjectManagement
+{
+	/// <summary>
+	/// Represents the object query.
+	/// </summary>
+	/// <typeparam name="TObject">The type of the object.</typeparam>
+	public class ObjectQueryAsync<TObject> : QueryAsync<TObject, ObjectQueryAsync<TObject>>
+	{
+		internal ObjectQueryAsync(ObjectQueryRequest request, IQueryExecutorAsync<TObject> executor)
+			: base(request, executor)
+		{
+		}
+
+		private new ObjectQueryRequest Request => (ObjectQueryRequest)base.Request;
+
+		/// <summary>
+		/// Sets the workspace ID for which the query should be executed.
+		/// </summary>
+		/// <param name="workspaceId">The workspace ID.</param>
+		/// <returns>The query object.</returns>
+		public ObjectQueryAsync<TObject> For(int workspaceId)
+		{
+			Request.WorkspaceId = workspaceId;
+
+			return this;
+		}
+
+		/// <summary>
+		/// Sets the user credentials with which the query should be executed.
+		/// </summary>
+		/// <param name="userCredentials">User credentials.</param>
+		/// <returns>The query object.</returns>
+		public ObjectQueryAsync<TObject> With(UserCredentials userCredentials)
+		{
+			Request.UserCredentials = userCredentials;
+
+			return this;
+		}
+	}
+}

ceb8a866d20159da858bf4c8e82de96f0b8f7ae1
Paul LeTang <paul.letang@relativity.com>
2021-08-02 11:37:02 -0700

    [TESTENG-1281] Updating the documentation for IObjectService to be more clear.

--- source/Relativity.Testing.Framework.Api/ObjectManagement/IObjectService.cs
+++ source/Relativity.Testing.Framework.Api/ObjectManagement/IObjectService.cs
@@ -14,160 +14,163 @@ namespace Relativity.Testing.Framework.Api.ObjectManagement
 		/// <summary>
 		/// Creates the specified workspace entity.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entity">The entity to update.</param>
-		/// <returns>Returns the <typeparamref name="TObject"/> object.</returns>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entity">The entity to create.</param>
+		/// <returns>Returns the <typeparamref name="TObject"/> entity.</returns>
 		TObject Create<TObject>(int workspaceId, TObject entity)
 			where TObject : Artifact;
 
 		/// <summary>
 		/// Creates the specified workspace entities.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entities">The array of entities.</param>
-		/// <returns>Returns the <typeparamref name="TObject"/> object.</returns>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entities">The array of entities to create.</param>
+		/// <returns>Returns a list of ArtifactIDs for the created entities.</returns>
 		List<int> Create<TObject>(int workspaceId, IEnumerable<TObject> entities)
 			where TObject : Artifact;
 
 		/// <summary>
 		/// Creates the specified workspace entity.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entity">The entity to update.</param>
-		/// <returns>Returns the task to to create the <typeparamref name="TObject"/> object.</returns>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entity">The entity to create.</param>
+		/// <returns>Returns the task to to create the <typeparamref name="TObject"/> entity.</returns>
 		Task<TObject> CreateAsync<TObject>(int workspaceId, TObject entity)
 			where TObject : Artifact;
 
 		/// <summary>
 		/// Creates the specified workspace entities.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entities">The array of entities.</param>
-		/// <returns>Returns the task to create the <typeparamref name="TObject"/> object.</returns>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entities">The array of entities to create.</param>
+		/// <returns>Returns the task to create the <typeparamref name="TObject"/> objects.</returns>
 		Task<List<int>> CreateAsync<TObject>(int workspaceId, IEnumerable<TObject> entities)
 			where TObject : Artifact;
 
 		/// <summary>
-		/// Updates specified fields on a list of Documents or Relativity Dynamic Objects (RDOs) that match a set of identifiers by setting the fields to the same value.
+		/// Updates the specified fields on a list of Documents or Relativity Dynamic Objects (RDOs) that match a set of identifiers by setting the fields to the same value.
 		/// </summary>
-		/// <param name="workspaceId">Workspace ID of the workspace containing the artifacts to be updated.</param>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
 		/// <param name="massRequestByObjectIdentifiers">A request to update multiple Documents or Relativity Dynamic Objects (RDOs) that have the specified identifiers.</param>
-		/// <remarks>A <see cref="MassUpdateByObjectIdentifiersRequest"/> object specifies the identifiers used to select a list of objects with the same type for updating. It also includes the same
-		/// values for all object fields that are to be updated. In the Relativity UI, this update operation is equivalent to the user selecting the Checked or These option in
-		/// the mass operations bar on a list page.
-		/// Process halts at first failure with no rollback.</remarks>
+		/// <remarks>
+		///     <para>A <see cref="MassUpdateByObjectIdentifiersRequest"/> object specifies the identifiers used to select a list of objects with the same type for updating.</para>
+		///     <para>It also includes the same values for all object fields that are to be updated.</para>
+		///     <para>In the Relativity UI, this update operation is equivalent to the user selecting the Checked or These option in the mass operations bar on a list page.</para>
+		///     <para>Process halts at first failure with no rollback.</para>
+		/// </remarks>
 		void Update(int workspaceId, MassUpdateByObjectIdentifiersRequest massRequestByObjectIdentifiers);
 
 		/// <summary>
 		/// Updates the specified workspace entity.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <param name="workspaceId">The workspace ID.</param>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
 		/// <param name="entity">The entity to update.</param>
 		void Update<TObject>(int workspaceId, TObject entity)
 			where TObject : Artifact;
 
 		/// <summary>
-		/// Updates specified fields on a list of Documents or Relativity Dynamic Objects (RDOs) that match a set of identifiers by setting the fields to the same value.
+		/// Updates the specified fields on a list of Documents or Relativity Dynamic Objects (RDOs) that match a set of identifiers by setting the fields to the same value.
 		/// </summary>
-		/// <param name="workspaceId">Workspace ID of the workspace containing the artifacts to be updated.</param>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
 		/// <param name="massRequestByObjectIdentifiers">A request to update multiple Documents or Relativity Dynamic Objects (RDOs) that have the specified identifiers.</param>
-		/// <remarks>A <see cref="MassUpdateByObjectIdentifiersRequest"/> object specifies the identifiers used to select a list of objects with the same type for updating. It also includes the same
-		/// values for all object fields that are to be updated. In the Relativity UI, this update operation is equivalent to the user selecting the Checked or These option in
-		/// the mass operations bar on a list page.
-		/// Process halts at first failure with no rollback.</remarks>
-		/// <returns>Returns the task to update the object.</returns>
+		/// <remarks>
+		///     <para>A <see cref="MassUpdateByObjectIdentifiersRequest"/> object specifies the identifiers used to select a list of objects with the same type for updating.</para>
+		///     <para>It also includes the same values for all object fields that are to be updated.</para>
+		///     <para>In the Relativity UI, this update operation is equivalent to the user selecting the Checked or These option in the mass operations bar on a list page.</para>
+		///     <para>Process halts at first failure with no rollback.</para>
+		/// </remarks>
+		/// <returns>Returns the task to update the entities.</returns>
 		Task UpdateAsync(int workspaceId, MassUpdateByObjectIdentifiersRequest massRequestByObjectIdentifiers);
 
 		/// <summary>
 		/// Updates the specified workspace entity.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <param name="workspaceId">The workspace ID.</param>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
 		/// <param name="entity">The entity to update.</param>
-		/// /// <returns>Returns the task to update the object.</returns>
+		/// <returns>Returns the task to update the entity.</returns>
 		Task UpdateAsync<TObject>(int workspaceId, TObject entity)
 			where TObject : Artifact;
 
 		/// <summary>
-		/// Deletes the workspace entity by the specified IDs of workspace and entity.
+		/// Deletes the workspace entity specified by the ArtifactIDs of the workspace and entity.
 		/// </summary>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entityId">The entity ID.</param>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entityId">The ArtifactID of the entity to delete.</param>
 		void Delete(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Deletes the workspace entities by the specified IDs of workspace and entities.
+		/// Deletes the workspace entities specified by the ArtifactIDs of the workspace and entities.
 		/// </summary>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entityIds">The entity IDs.</param>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entityIds">The collection of ArtifactIDs of the entities to delete.</param>
 		void Delete(int workspaceId, IEnumerable<int> entityIds);
 
 		/// <summary>
-		/// Deletes the workspace entity by the specified IDs of workspace and entity.
+		/// Deletes the workspace entity specified by the ArtifactIDs of the workspace and entity.
 		/// </summary>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entityId">The entity ID.</param>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entityId">The ArtifactID of the entity to delete.</param>
 		/// <returns>The task to delete the entity.</returns>
 		Task DeleteAsync(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Deletes the workspace entities by the specified IDs of workspace and entities.
+		/// Deletes the workspace entities specified by the ArtifactIDs of the workspace and entities.
 		/// </summary>
-		/// <param name="workspaceId">The workspace ID.</param>
-		/// <param name="entityIds">The entity IDs.</param>
-		/// <returns>The task to delete the entity.</returns>
+		/// <param name="workspaceId">The ArtifactID of the workspace.</param>
+		/// <param name="entityIds">The collection of ArtifactIDs of the entities to delete.</param>
+		/// <returns>The task to delete the entities.</returns>
 		Task DeleteAsync(int workspaceId, IEnumerable<int> entityIds);
 
 		/// <summary>
-		/// Gets all objects of <typeparamref name="TObject"/> type.
+		/// Gets all entities of the <typeparamref name="TObject"/> type.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <returns>The collection of <typeparamref name="TObject"/> objects.</returns>
+		/// <typeparam name="TObject">The type of the entities.</typeparam>
+		/// <returns>The collection of <typeparamref name="TObject"/> entities.</returns>
 		TObject[] GetAll<TObject>();
 
 		/// <summary>
-		/// Gets all objects of <typeparamref name="TObject"/> type matching specified property filter.
+		/// Gets all entities of the <typeparamref name="TObject"/> type matching the specified property filter.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
+		/// <typeparam name="TObject">The type of the entities.</typeparam>
 		/// <param name="wherePropertySelector">The filter property selector.</param>
 		/// <param name="whereValue">The filter property value.</param>
-		/// <returns>The collection of <typeparamref name="TObject"/> objects.</returns>
+		/// <returns>The collection of <typeparamref name="TObject"/> entities that match the filter.</returns>
 		TObject[] GetAll<TObject>(Expression<Func<TObject, object>> wherePropertySelector, object whereValue);
 
 		/// <summary>
-		/// Gets all objects of <typeparamref name="TObject"/> type.
+		/// Gets all entities of the <typeparamref name="TObject"/> type.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <returns>The collection of <typeparamref name="TObject"/> objects.</returns>
-		/// <returns>The task to get all entities of <typeparamref name="TObject"/> type.</returns>
+		/// <typeparam name="TObject">The type of the entities.</typeparam>
+		/// <returns>The task to get all entities of the <typeparamref name="TObject"/> type.</returns>
 		Task<TObject[]> GetAllAsync<TObject>();
 
 		/// <summary>
-		/// Gets all objects of <typeparamref name="TObject"/> type matching specified property filter.
+		/// Gets all entities of the <typeparamref name="TObject"/> type matching the specified property filter.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
+		/// <typeparam name="TObject">The type of the entities.</typeparam>
 		/// <param name="wherePropertySelector">The filter property selector.</param>
 		/// <param name="whereValue">The filter property value.</param>
-		/// <returns>The task to get all entities of <typeparamref name="TObject"/> type.</returns>
+		/// <returns>The task to get the collection of <typeparamref name="TObject"/> entities that match the filter.</returns>
 		Task<TObject[]> GetAllAsync<TObject>(Expression<Func<TObject, object>> wherePropertySelector, object whereValue);
 
 		/// <summary>
 		/// Gets a query to enumerate <typeparamref name="TObject"/> objects.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <returns>The <see cref="ObjectQuery{TObject}"/> object.</returns>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <returns>The <see cref="ObjectQuery{TObject}"/> operator.</returns>
 		ObjectQuery<TObject> Query<TObject>();
 
 		/// <summary>
-		/// Gets a query to enumerate <typeparamref name="TObject"/> objects.
+		/// Gets an asynchronous query to enumerate <typeparamref name="TObject"/> objects.
 		/// </summary>
-		/// <typeparam name="TObject">The type of the object.</typeparam>
-		/// <returns>The task to query <see cref="ObjectQueryAsync{TObject}"/> object.</returns>
+		/// <typeparam name="TObject">The type of the entity.</typeparam>
+		/// <returns>The query <see cref="ObjectQueryAsync{TObject}"/> operator.</returns>
 		ObjectQueryAsync<TObject> AsyncQuery<TObject>();
 	}
 }

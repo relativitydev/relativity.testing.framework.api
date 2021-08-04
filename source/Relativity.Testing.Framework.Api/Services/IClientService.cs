cb73a53d99e289e765b2d38de0a894e70f4ce55c
Paul LeTang <paul.letang@relativity.com>
2021-08-04 10:57:05 -0700

    [RTF-1275] Adding examples for IClientService.

--- source/Relativity.Testing.Framework.Api/Services/IClientService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IClientService.cs
@@ -6,22 +6,47 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the client API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// _clientService = relativityFacade.Resolve&lt;IClientService&gt;();
+	/// </code>
+	/// </example>
 	public interface IClientService
 	{
 		/// <summary>
 		/// Creates the specified client.
 		/// </summary>
-		/// <param name="entity">The entity to create.</param>
-		/// <returns>The created entity.</returns>
+		/// <param name="entity">The <see cref="Client"/> to create.</param>
+		/// <returns>The created client.</returns>
+		/// <example>
+		/// Create any old client.
+		/// <code>
+		/// Client client = _clientService.Create(Client());
+		/// </code>
+		/// </example>
+		/// <example>
+		/// Create a client with specified properties.
+		/// <code>
+		/// Client client = new Client
+		/// {
+		///     Name = "TheBigClient",
+		///     Number = 12345,
+		///     Status = new NamedArtifact { Name = ClientStatus.Inactive.ToString() },
+		///     Keywords = "SomeKeyword"
+		///     Notes = "Some note about the client."
+		/// };
+		/// Client client = _clientService.Create(client);
+		/// </code>
+		/// </example>
 		Client Create(Client entity);
 
 		/// <summary>
 		/// Updates the specified <see cref="Client"/>.
 		/// </summary>
-		/// <param name="entity">Updated <see cref="Client"/> entity.</param>
+		/// <param name="entity">The <see cref="Client"/> model to use to update the existing client with.</param>
 		/// <example>
 		/// <code>
-		/// var client = _clientService.Get("Some Client Name");
+		/// Client client = _clientService.Get("Some Client Name");
 		/// client.Keywords = "SampleKeywords";
 		/// _clientService.Update(client);
 		/// </code>
@@ -31,33 +56,72 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <summary>
 		/// Requires the specified client.
 		/// <list type="number">
-		/// <item>If <see cref="Artifact.ArtifactID"/> property of <paramref name="entity"/> has positive value, gets entity by ID and returns it.</item>
-		/// <item>If <see cref="NamedArtifact.Name"/> property of <paramref name="entity"/> has a value, gets entity by name and returns it if it exists.</item>
-		/// <item>Otherwise creates a new entity using <see cref="ICreateStrategy{T}"/>.</item>
+		/// <item>If the <see cref="Artifact.ArtifactID"/> property of <paramref name="entity"/> has a positive value, this gets entity by ID and returns it.</item>
+		/// <item>Else if the <see cref="NamedArtifact.Name"/> property of <paramref name="entity"/> has a value, this gets the entity by name and returns it if it exists.</item>
+		/// <item>Otherwise this creates a new entity using <see cref="ICreateStrategy{T}"/>.</item>
 		/// </list>
 		/// </summary>
-		/// <param name="entity">The entity to require.</param>
-		/// <returns>The entity required.</returns>
+		/// <param name="entity">The <see cref="Client"/> to require.</param>
+		/// <returns>The created or updated <see cref="Client"/>.</returns>
+		/// <example>
+		/// <para>This example will search for a client with the ArtifactID "1234567".</para>
+		/// <para>If it is found, it will be returned.</para>
+		/// <para>If not, it will be created.</para>
+		/// <code>
+		/// Client client = new Client
+		/// {
+		///     ArtifactID = 1234567
+		/// };
+		/// client = _clientService.Require(client);
+		/// </code>
+		/// </example>
+		/// <example>
+		/// <para>This example will search for a client named "MyClient".</para>
+		/// <para>If one is found, it will be returned.</para>
+		/// <para>If not, it will be created.</para>
+		/// <code>
+		/// Client client = new Client
+		/// {
+		///     Name = "MyClient"
+		/// };
+		/// client = _clientService.Require(client);
+		/// </code>
+		/// </example>
 		Client Require(Client entity);
 
 		/// <summary>
-		/// Deletes the client by ID.
+		/// Deletes the <see cref="Client"/> by ID.
 		/// </summary>
-		/// <param name="id">The artifact ID of the client.</param>
+		/// <param name="id">The ArtifactID of the client.</param>
+		/// <example>
+		/// <code>
+		/// _clientService.Delete(client.ArtifactID);
+		/// </code>
+		/// </example>
 		void Delete(int id);
 
 		/// <summary>
-		/// Gets the client by the specified ID.
+		/// Gets the <see cref="Client"/> by ArtifactID.
 		/// </summary>
-		/// <param name="id">The artifact ID of the client.</param>
-		/// <returns>The <see cref="Client"/> entity or <see langword="null"/>.</returns>
+		/// <param name="id">The ArtifactID of the client.</param>
+		/// <returns>The <see cref="Client"/> entity if it exists, else <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// Client client = _clientService.Get(1234567);
+		/// </code>
+		/// </example>
 		Client Get(int id);
 
 		/// <summary>
-		/// Gets the client by the specified name.
+		/// Gets the <see cref="Client"/> by name.
 		/// </summary>
-		/// <param name="name">The name.</param>
-		/// <returns>The <see cref="Client"/> entity or <see langword="null"/>.</returns>
+		/// <param name="name">The name of the client.</param>
+		/// <returns>The <see cref="Client"/> entity if it exists, else <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// Client client = _clientService.Get("AnotherClient");
+		/// </code>
+		/// </example>
 		Client Get(string name);
 	}
 }

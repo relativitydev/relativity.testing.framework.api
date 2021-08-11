87c83d40efabcfb981318dd731485fb83a6cd88b
Sai Nissankula <sairaghav.nissankula@relativity.com>
2021-08-11 15:55:01 -0500

    TESTENG-1404: Add DocumentImportHelper functional test

---
+++ source/Relativity.Testing.Framework.Api.FunctionalTests/Strategies/Documents/DocumentImportHelperFixture.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using NUnit.Framework;
+using Relativity.Testing.Framework.Api.Exceptions;
+using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Api.Strategies;
+
+namespace Relativity.Testing.Framework.Api.FunctionalTests.Strategies.Documents
+{
+    [TestOf(typeof(DocumentImportHelper))]
+	internal class DocumentImportHelperFixture : ApiServiceTestFixture<IDocumentSingleNativeImportStrategy>
+	{
+		[Test]
+		public void DocumentImportHelper_WhenInvalidDataTable_ThrowsJobReportException()
+		{
+			var documentService = Facade.Resolve<IDocumentService>();
+			DataTable dataTable = new DataTable();
+			Assert.Throws<JobReportException>(() => documentService.ImportNatives(DefaultWorkspace.ArtifactID, dataTable));
+			dataTable.Dispose();
+		}
+	}
+}

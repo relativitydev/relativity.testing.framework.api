fe0f83c9b160bcc65fbd72e26e97f077849af656
matthew-wiemer <72028853+matthew-wiemer@users.noreply.github.com>
2021-09-01 16:14:11 -0500

    Rtf 1392 Updating tests to run SunDrop, and a bugfix for allowing 12.3 tests to run at all.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+
+## [4.6.0] - 2021-09-01
+
+### Changed
+
+- Consumers can now use the ApiComponent against Relativity 12.3
+
 ## [4.5.2] - 2021-09-01
 
 ### Changed
---
+++ DevelopmentScripts/Invoke-FunctionalTest.ps1
@@ -0,0 +1,77 @@
+param(
+    $ClientID,
+    $ClientSecret,
+    $DirectoryID,
+    $VirtualMachineTag,
+    $HopperAPIURL,
+    $HopperUsername,
+    $BuildOwner,
+    $ProductName,
+    $SUTVersion
+)
+
+$Body = @{
+  grant_type = "client_credentials"
+  client_id = "$ClientID"
+  client_secret = "$ClientSecret"
+  scope = "https://vault.azure.net/.default"
+}
+
+$AccessToken = (Invoke-RestMethod -Method POST `
+  -Uri "https://login.microsoftonline.com/$DirectoryID/oauth2/v2.0/token" `
+  -Body $Body).access_token
+
+$HopperApiKey = (Invoke-RestMethod -Method GET `
+  -Uri "https://testengineering-github.vault.azure.net/secrets/HopperApiKey?api-version=7.1" `
+  -Headers @{ Authorization="Bearer $AccessToken" }).value
+
+Import-Module ./HopperPowerShellClient/*/HopperPowerShellClient.psm1
+
+$VirtualMachineName = "$VirtualMachineTag.$SUTVersion"
+
+$Instance = New-Instance -ApiUrl "$HopperAPIURL" `
+  -ApiKey $HopperApiKey `
+  -ApiUsername "$HopperUsername" `
+  -TemplateName $SUTVersion `
+  -VmName "$VirtualMachineName" `
+  -Description "$VirtualMachineName" `
+  -Tags @{"Build Owner" = "$BuildOwner"; "Product Name" = "$ProductName"} `
+  -TargetRegion 3 `
+  -Referrer "https://testengineering.relativity.com/" `
+  -Verbose `
+  -PassThru
+
+if(!$?) {throw "An error ocurred while creating the instance in Hopper. Please check the logs."}
+
+Start-Sleep 180 # Environments are less likely to throw 500s if they are up for a bit before running tests.
+
+$Credentials = $Instance.Credentials | Where-Object { $_.CredentialType.Name -eq "Relativity" }
+$RelativityHostAddress = "$($instance.ServiceName).relativityhopper.com"
+
+./New-TestSettings.ps1 -ServerBindingType "https" `
+  -RelativityHostAddress $RelativityHostAddress `
+  -AdminUsername $Credentials.Username `
+  -AdminPassword $Credentials.Password `
+  -RunSettingsPrefix $SUTVersion
+
+$TestSettings = "$($SUTVersion)FunctionalTest.runsettings"
+$LogFilePath = Join-Path (Get-Location) "../Artifacts/Logs/{assembly}.{framework}.$($SUTVersion)TestResults.xml"
+$ResultsPath = Join-Path (Get-Location) "../Artifacts/Logs/$SUTVersion"
+
+# This sets the location for the trace logs for the tests.
+$Env:ResultsLocation = $ResultsPath
+
+dotnet test ..\Tests\Relativity.Testing.Framework.Api.FunctionalTests.dll `
+  --nologo `
+  --ResultsDirectory $ResultsPath `
+  "--logger:nunit;LogFilePath=$LogFilePath" `
+  -s $TestSettings
+
+Remove-Instance -ApiUrl "$HopperAPIURL" `
+  -ApiKey $HopperApiKey `
+  -ApiUsername "$HopperUsername" `
+  -Id $Instance.Id `
+  -Referrer "https://testengineering.relativity.com/" `
+  -Verbose
+
+if(!$?) {throw "An error ocurred while deleting the Hopper instance. Please check the logs."}

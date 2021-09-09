7b80eba7f90776b784c185ff85010e07e809f267
Paul Letang <71905764+paulletang@users.noreply.github.com>
2021-09-09 08:44:57 -0500

    [TESTENG-1530] Updating error message in waitUserDeleted to be more explicit about the timeout. (#148)

--- source/Relativity.Testing.Framework.Api.Tests/Services/AccountPoolServiceFixture.cs
+++ source/Relativity.Testing.Framework.Api.Tests/Services/AccountPoolServiceFixture.cs
@@ -17,22 +17,28 @@ namespace Relativity.Testing.Framework.Api.Tests.Services
 		private readonly Mock<ICreateStrategy<User>> _mockUserCreateStrategy = new Mock<ICreateStrategy<User>>();
 		private readonly Mock<IUserExistsByEmailStrategy> _mockUserExistsByEmailStrategy = new Mock<IUserExistsByEmailStrategy>();
 		private readonly Mock<IUserGetByEmailStrategy> _mockUserGetByEmailStrategy = new Mock<IUserGetByEmailStrategy>();
-		private readonly Mock<IWaitUserDeletedStrategy> _mockWaitUserDeletedStrategy = new Mock<IWaitUserDeletedStrategy>();
 		private readonly Mock<IDeleteByIdStrategy<User>> _mockUserDeleteByIdStrategy = new Mock<IDeleteByIdStrategy<User>>();
 		private readonly Mock<IGetAllByNamesStrategy<Group>> _mockGroupGetAllByNamesStrategy = new Mock<IGetAllByNamesStrategy<Group>>();
 		private readonly Mock<IObjectService> _mockObjectService = new Mock<IObjectService>();
+		private readonly Mock<IGetByIdStrategy<User>> _mockUserGetByIdStrategy = new Mock<IGetByIdStrategy<User>>();
 
+		private IWaitUserDeletedStrategy _waitUserDeletedStrategy;
 		private AccountPoolService _sut;
 
 		[SetUp]
 		public void Setup()
 		{
+			_waitUserDeletedStrategy = new WaitUserDeletedStrategy(
+				_mockUserGetByEmailStrategy.Object,
+				_mockUserGetByIdStrategy.Object,
+				TimeSpan.FromSeconds(3));
+
 			_sut = new AccountPoolService(
 				_mockLogService.Object,
 				_mockUserCreateStrategy.Object,
 				_mockUserExistsByEmailStrategy.Object,
 				_mockUserGetByEmailStrategy.Object,
-				_mockWaitUserDeletedStrategy.Object,
+				_waitUserDeletedStrategy,
 				_mockUserDeleteByIdStrategy.Object,
 				_mockGroupGetAllByNamesStrategy.Object,
 				_mockObjectService.Object);
@@ -44,12 +50,21 @@ namespace Relativity.Testing.Framework.Api.Tests.Services
 			_mockUserGetByEmailStrategy.SetupSequence(x => x.Get(It.IsAny<string>()))
 				.Returns(new User())
 				.Returns(new User())
-				.Returns(new User())
-				.Returns(new User())
 				.Returns(null);
 
 			Assert.DoesNotThrow(() => _sut.DeleteAndAcquireStandardAccount());
 			_mockUserCreateStrategy.Verify(x => x.Create(It.IsAny<User>()), Times.Once);
+		}
+
+		[Test]
+		public void WaitDeleteStandardAccount_ShouldThrowCustomExceptionIfDeleteRequestIsAcceptedButUserIsNotDeleted()
+		{
+			_mockUserGetByEmailStrategy.Setup(x => x.Get(It.IsAny<string>())).Returns(new User());
+
+			Assert.That(
+			    () => _sut.WaitDeleteStandardAccount("a"),
+			    Throws.TypeOf<InvalidOperationException>()
+				.With.Message.Contains("The request to delete the "));
 		}
 	}
 }

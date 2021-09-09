bd2ca177ed19e055d15166c90650c21ffad85557
jonatanjakoniuk <81292620+jonatanjakoniuk@users.noreply.github.com>
2021-09-09 11:34:32 +0200

    Added Get default field values for a placeholder method to ProductionPlaceholderService (#150)

---
+++ source/Relativity.Testing.Framework.Api.FunctionalTests/Strategies/Productions/ProductionPlaceholderGetDefaultFieldValuesStrategyFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Relativity.Testing.Framework.Api.Strategies;
+
+namespace Relativity.Testing.Framework.Api.FunctionalTests.Strategies
+{
+	[TestOf(typeof(IProductionPlaceholderGetDefaultFieldValuesStrategy))]
+	internal class ProductionPlaceholderGetDefaultFieldValuesStrategyFixture : ApiServiceTestFixture<IProductionPlaceholderGetDefaultFieldValuesStrategy>
+	{
+		[Test]
+		public void Get_ShouldNotThrown()
+		{
+			Assert.DoesNotThrow(() => Sut.Get(DefaultWorkspace.ArtifactID));
+		}
+	}
+}
---
+++ source/Relativity.Testing.Framework.Api/Strategies/Productions/ProductionsPlaceholder/DTO/ChoiceFieldValueDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Relativity.Testing.Framework.Api.Strategies
+{
+	internal class ChoiceFieldValueDto
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+	}
+}

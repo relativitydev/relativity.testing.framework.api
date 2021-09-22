d22d7814002544a1fade42c6cf7e01634885e8ed
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-09-22 16:02:34 +0200

    RTF-1302 - Add examples for ISearchProviderService

--- source/Relativity.Testing.Framework.Api/Services/ISearchProviderService.cs
+++ source/Relativity.Testing.Framework.Api/Services/ISearchProviderService.cs
@@ -7,6 +7,11 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the search provider API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// ISearchProviderService _searchProviderService = relativityFacade.Resolve&lt;ISearchProviderService&gt;();
+	/// </code>
+	/// </example>
 	public interface ISearchProviderService
 	{
 		/// <summary>
@@ -15,6 +20,17 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to add the new search provider.</param>
 		/// <param name="entity">The entity to create.</param>
 		/// <returns>The created entity.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// var searchProvider = new SearchProvider
+		/// {
+		///     Name = "SomeName",
+		///     Order = 200
+		/// };
+		/// SearchProvider createdSearchProvider = _searchProviderService.Create(workspaceID, searchProvider);
+		/// </code>
+		/// </example>
 		SearchProvider Create(int workspaceId, SearchProvider entity);
 
 		/// <summary>
@@ -22,6 +38,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to delete the search provider.</param>
 		/// <param name="entityId">The artifact ID of the search provider.</param>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// int searchProviderID = 654321;
+		///
+		/// _searchProviderService.Delete(workspaceID, searchProviderID);
+		/// </code>
+		/// </example>
 		void Delete(int workspaceId, int entityId);
 
 		/// <summary>
@@ -30,6 +54,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to get search provider.</param>
 		/// <param name="entityId">The artifact ID of the search provider.</param>
 		/// <returns>The <see cref="KeywordSearch"/> entity or <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// int searchProviderID = 654321;
+		///
+		/// SearchProvider searchProvider = _searchProviderService.Get(workspaceID, searchProviderID);
+		/// </code>
+		/// </example>
 		SearchProvider Get(int workspaceId, int entityId);
 
 		/// <summary>
@@ -38,6 +70,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to get search provider.</param>
 		/// <param name="entityName">The name of the search provider.</param>
 		/// <returns>The <see cref="SearchProvider"/> entity or <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// int searchProviderName = "searchProviderName";
+		///
+		/// SearchProvider searchProvider = _searchProviderService.Get(workspaceID, searchProviderName);
+		/// </code>
+		/// </example>
 		SearchProvider Get(int workspaceId, string entityName);
 
 		/// <summary>
@@ -45,6 +85,18 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to update search provider.</param>
 		/// <param name="entity">The entity to update.</param>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// int searchProviderID = 654321;
+		///
+		/// SearchProvider searchProvider = _searchProviderService.Get(workspaceID, searchProviderID);
+		/// searchProvider.Name = "New name";
+		/// searchProvider.Order = 10;
+		///
+		/// _searchProviderService.Update(workspaceID, searchProvider);
+		/// </code>
+		/// </example>
 		void Update(int workspaceId, SearchProvider entity);
 
 		/// <summary>
@@ -58,6 +110,18 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <returns>The <see cref="SearchProvider"/> entity or <see langword="null"/>.</returns>
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to require search provider.</param>
 		/// <param name="entity">The entity to require.</param>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// var searchProvider = new SearchProvider
+		/// {
+		///     Name = "SomeName",
+		///     Order = 200
+		/// };
+		///
+		/// SearchProvider result = _searchProviderService.Require(workspaceID, searchProvider);
+		/// </code>
+		/// </example>
 		SearchProvider Require(int workspaceId, SearchProvider entity);
 
 		/// <summary>
@@ -66,6 +130,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The workspace ID.</param>
 		/// <param name="entityId">The entity ID.</param>
 		/// <returns>The list of dependencies.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 1234567;
+		/// int searchProviderID = 654321;
+		///
+		/// List&lt;Dependency&gt; dependencies = _searchProviderService.GetDependencies(workspaceID, searchProviderID);
+		/// </code>
+		/// </example>
 		List<Dependency> GetDependencies(int workspaceId, int entityId);
 	}
 }

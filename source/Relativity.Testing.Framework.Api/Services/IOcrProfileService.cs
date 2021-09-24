779ca3685d8a88de9f3f2c82bb2fe9560aa47b34
klaudiaknafel <81292389+klaudiaknafel@users.noreply.github.com>
2021-09-24 12:25:08 +0200

    [RTF-1293] Adding IOcrProfileService docs examples (#186)

--- source/Relativity.Testing.Framework.Api/Services/IOcrProfileService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IOcrProfileService.cs
@@ -5,32 +5,68 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the optical character recognition API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// IOcrProfileService _ocrProfileService = relativityFacade.Resolve&lt;IOcrProfileService&gt;();
+	/// </code>
+	/// </example>
 	public interface IOcrProfileService
 	{
 		/// <summary>
-		/// Creates the specified OCR profile.
+		/// Creates the specified <see cref="OcrProfile"/>.
 		/// </summary>
-		/// <param name="workspaceId">The Artifact ID of the workspace where you want to add the new OCR profile,
+		/// <param name="workspaceId">The ArtifactID of the workspace where you want to add the <see cref="OcrProfile"/>,
 		/// or use -1 to indicate the admin-level context.</param>
-		/// <param name="entity">The entity to create.</param>
-		/// <returns>The created entity.</returns>
+		/// <param name="entity">The <see cref="OcrProfile"/>entity to create.</param>
+		/// <returns>The created <see cref="OcrProfile"/> entity.</returns>
+		/// <example> This example shows how to create OCR Profile with some properties set.
+		/// <code>
+		/// int workspaceArtifactID = 1015427;
+		/// var ocrProfileToCreate = new OcrProfile
+		/// {
+		/// 	Name = "My script Name",
+		/// 	ImageTimeout = 120
+		/// };
+		/// OcrProfile ocrProfile = _ocrProfileService.Create(workspaceArtifactID, ocrProfileToCreate);
+		/// </code>
+		/// </example>
+		/// <example> This example shows how to create OCR Profile without setting any properties - default values will be assigned.
+		/// <code>
+		/// int workspaceArtifactID = 1015427;
+		/// OcrProfile ocrProfile = _ocrProfileService.Create(workspaceArtifactID, new OcrProfile());
+		/// </code>
+		/// </example>
 		OcrProfile Create(int workspaceId, OcrProfile entity);
 
 		/// <summary>
-		/// Deletes the OCR profile by ID.
+		/// Deletes the <see cref="OcrProfile"/> by ArtifactID.
 		/// </summary>
-		/// <param name="workspaceId">The Artifact ID of the workspace where you want to delete the OCR profile,
+		/// <param name="workspaceId">The ArtifactID of the workspace where you want to delete the <see cref="OcrProfile"/>,
 		/// or use -1 to indicate the admin-level context.</param>
-		/// <param name="entityId">The artifact ID of the OCR profile.</param>
+		/// <param name="entityId">The ArtifactID of the <see cref="OcrProfile"/>.</param>
+		/// <example>
+		/// <code>
+		/// int workspaceArtifactID = 1015427;
+		/// int existingOCRProfileArtifactID = 1015427;
+		/// _ocrProfileService.Delete(workspaceArtifactID, existingOCRProfileArtifactID);
+		/// </code>
+		/// </example>
 		void Delete(int workspaceId, int entityId);
 
 		/// <summary>
-		/// Gets the OCR profile by the specified ID.
+		/// Gets the <see cref="OcrProfile"/> by the specified ArtifactID.
 		/// </summary>
-		/// <param name="workspaceId">The Artifact ID of the workspace where you want to get the OCR profile,
+		/// <param name="workspaceId">The ArtifactID of the workspace where you want to get the <see cref="OcrProfile"/>,
 		/// or use -1 to indicate the admin-level context.</param>
-		/// <param name="entityId">The artifact ID of the OCR profile.</param>
-		/// <returns>>The <see cref="ObjectType"/> entity or <see langword="null"/>.</returns>
+		/// <param name="entityId">The ArtifactID of the <see cref="OcrProfile"/>.</param>
+		/// <returns>>The <see cref="OcrProfile"/> entity or <see langword="null"/>.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceArtifactID = 1015427;
+		/// int existingOCRProfileArtifactID = 1015427;
+		/// OcrProfile ocrProfile = _ocrProfileService.Get(workspaceArtifactID, existingOCRProfileArtifactID);
+		/// </code>
+		/// </example>
 		OcrProfile Get(int workspaceId, int entityId);
 	}
 }

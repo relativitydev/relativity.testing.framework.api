015b970839a057aa5e8e78b40561284209e17664
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-09-24 10:13:53 +0200

    RTF-1295 - Add examples for IProductionDataSourceService

--- source/Relativity.Testing.Framework.Api/Services/IProductionDataSourceService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IProductionDataSourceService.cs
@@ -5,6 +5,11 @@ namespace Relativity.Testing.Framework.Api.Services
 	/// <summary>
 	/// Represents the production data source API service.
 	/// </summary>
+	/// <example>
+	/// <code>
+	/// IProductionDataSourceService _productionDataSourceService = RelativityFacade.Resolve&lt;IProductionDataSourceService&gt;();
+	/// </code>
+	/// </example>
 	public interface IProductionDataSourceService
 	{
 		/// <summary>
@@ -14,6 +19,27 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="productionId">The Artifact ID of production.</param>
 		/// <param name="entity">The data source entity to create.</param>
 		/// <returns>The created data source entity.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 123456;
+		///
+		/// Production _production = Facade.Resolve&lt;ICreateWorkspaceEntityStrategy&lt;Production&gt;&gt;()
+		/// 	.Create(workspaceID, new Production());
+		/// KeywordSearch _keywordSearch = Facade.Resolve&lt;ICreateWorkspaceEntityStrategy&lt;KeywordSearch&gt;&gt;()
+		/// 	.Create(workspaceID, new KeywordSearch());
+		///
+		/// ProductionDataSource toCreate = new ProductionDataSource
+		/// {
+		/// 	Name = "Some name",
+		/// 	ProductionId = _production.ArtifactID,
+		/// 	ProductionType = ProductionType.NativesOnly,
+		/// 	SavedSearch = new NamedArtifact { ArtifactID = _keywordSearch.ArtifactID },
+		/// 	UseImagePlaceholder = UseImagePlaceholderOption.NeverUseImagePlaceholder
+		/// };
+		///
+		/// ProductionDataSource result = _productionDataSourceService.Create(workspaceID, _production.ArtifactID, toCreate);
+		/// </code>
+		/// </example>
 		ProductionDataSource Create(int workspaceId, int productionId, ProductionDataSource entity);
 
 		/// <summary>
@@ -22,6 +48,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace.</param>
 		/// <param name="entityId">The Artifact ID of the production data source.</param>
 		/// <returns><see langword="true"/> if a production data source exists; otherwise, <see langword="false"/>.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 123456;
+		/// int productionDataSourceID  = 198715;
+		///
+		/// bool result = _productionDataSourceService.Exists(workspaceID, productionDataSourceID);
+		/// </code>
+		/// </example>
 		bool Exists(int workspaceId, int entityId);
 
 		/// <summary>
@@ -29,6 +63,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// </summary>
 		/// <param name="workspaceId">The Artifact ID of the workspace.</param>
 		/// <param name="entityId">The Artifact ID of the production data source.</param>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 123456;
+		/// int productionDataSourceID  = 198715;
+		///
+		/// _productionDataSourceService.Delete(workspaceID, productionDataSourceID);
+		/// </code>
+		/// </example>
 		void Delete(int workspaceId, int entityId);
 
 		/// <summary>
@@ -37,6 +79,14 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The workspace ID.</param>
 		/// <param name="entityId">The entity ID.</param>
 		/// <returns>The entity.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 123456;
+		/// int productionDataSourceID = 198715;
+		///
+		/// ProductionDataSource productionDataSource = _productionDataSourceService.Get(workspaceID, productionDataSourceID);
+		/// </code>
+		/// </example>
 		ProductionDataSource Get(int workspaceId, int entityId);
 
 		/// <summary>
@@ -45,6 +95,17 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="workspaceId">The Artifact ID of the workspace where you want to update the production data source.</param>
 		/// <param name="productionId">The Artifact ID of production.</param>
 		/// <param name="entity">The entity to update.</param>
+		/// <example>
+		/// <code>
+		/// int workspaceID = 123456;
+		/// int productionDataSourceID = 198715;
+		///
+		/// ProductionDataSource toUpdate = _productionDataSourceService.Get(workspaceID, productionDataSourceID);
+		/// toUpdate.Name = "New name";
+		///
+		/// _productionDataSourceService.Update(workspaceID, toUpdate.ProductionId, toUpdate);
+		/// </code>
+		/// </example>
 		void Update(int workspaceId, int productionId, ProductionDataSource entity);
 	}
 }

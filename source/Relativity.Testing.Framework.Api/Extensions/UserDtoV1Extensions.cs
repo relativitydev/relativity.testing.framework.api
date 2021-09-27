b64d07460da5fdc0ff87d9c68893717b5c934eb5
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-09-27 15:14:07 +0200

    RTF-1397 - Create new version of Get User strategy

---
+++ source/Relativity.Testing.Framework.Api/Extensions/UserDtoV1Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Relativity.Testing.Framework.Api.Strategies;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Extensions
+{
+	internal static class UserDtoV1Extensions
+	{
+		public static User MapToUser(this UserDtoV1 userDto, Client client)
+		{
+			return new User
+			{
+				ArtifactID = userDto.ArtifactID,
+				FirstName = userDto.FirstName,
+				LastName = userDto.LastName,
+				EmailAddress = userDto.EmailAddress,
+				EmailPreference = userDto.EmailPreference,
+				Type = userDto.Type.Name,
+				ItemListPageLength = userDto.ItemListPageLength,
+				Client = client,
+				DefaultSelectedFileType = userDto.DocumentViewerProperties.DefaultSelectedFileType,
+				ChangeSettings = userDto.AllowSettingsChange,
+				TrustedIPs = userDto.TrustedIPs,
+				RelativityAccess = userDto.RelativityAccess,
+				KeyboardShortcuts = userDto.DocumentViewerProperties.AllowKeyboardShortcuts,
+				DocumentViewer = userDto.DocumentViewerProperties.DocumentViewer,
+				FullName = $"{userDto.FirstName}, {userDto.LastName}",
+				BetaUser = false,
+				AdvancedSearchPublicByDefault = false,
+				NativeViewerCacheAhead = false,
+				ChangePassword = false,
+				MaximumPasswordAge = 90,
+				ChangePasswordNextLogin = true,
+				Password = string.Empty,
+				DocumentSkip = UserDocumentSkip.Enabled,
+				DataFocus = 5,
+				EnforceViewerCompatibility = false,
+				SkipDefaultPreference = UserSkipDefaultPreference.Skip,
+				CanChangeDocumentViewer = true,
+				DisableOnDate = DateTime.UtcNow.Date.AddYears(1).AddHours(8).AddMinutes(45).AddSeconds(30),
+			};
+		}
+	}
+}
---
+++ source/Relativity.Testing.Framework.Api/Strategies/Users/UserGetByIdStrategyV1.cs
@@ -0,0 +1,28 @@
+﻿using Relativity.Testing.Framework.Api.Extensions;
+using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Models;
+using Relativity.Testing.Framework.Versioning;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	[VersionRange(">=12.1")]
+	internal class UserGetByIdStrategyV1 : IGetByIdStrategy<User>
+	{
+		private readonly IRestService _restService;
+		private readonly IGetByIdStrategy<Client> _clientGetByIdStrategy;
+
+		public UserGetByIdStrategyV1(IRestService restService, IGetByIdStrategy<Client> clientGetByIdStrategy)
+		{
+			_restService = restService;
+			_clientGetByIdStrategy = clientGetByIdStrategy;
+		}
+
+		public User Get(int id)
+		{
+			var userDto = _restService.Get<UserDtoV1>($"Relativity-Identity/v1/users/{id}");
+			var client = _clientGetByIdStrategy.Get(userDto.Client.Value.ArtifactID);
+
+			return userDto.MapToUser(client);
+		}
+	}
+}

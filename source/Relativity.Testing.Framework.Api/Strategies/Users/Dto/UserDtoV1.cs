9d41695fe68aac57a9b975d4bdfc1ed1f36707d2
jonatanjakoniuk <81292620+jonatanjakoniuk@users.noreply.github.com>
2021-09-30 11:54:57 +0200

    RTF-1397 - Create new version of Get User strategy (#195)

---
+++ source/Relativity.Testing.Framework.Api/Strategies/Users/Dto/UserDtoV1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	internal class UserDtoV1
+	{
+		public int ArtifactID { get; set; }
+
+		public List<Guid> Guids { get; set; } = new List<Guid>();
+
+		public bool AllowSettingsChange { get; set; }
+
+		public Securable<NamedArtifact> Client { get; set; }
+
+		public bool DefaultFilterVisibility { get; set; }
+
+		public DateTime? DisableOnDate { get; set; }
+
+		public DocumentViewerPropertiesDtoV1 DocumentViewerProperties { get; set; }
+
+		public string EmailAddress { get; set; }
+
+		public UserEmailPreference EmailPreference { get; set; }
+
+		public string FirstName { get; set; }
+
+		public int ItemListPageLength { get; set; }
+
+		public string LastName { get; set; }
+
+		public bool RelativityAccess { get; set; }
+
+		public bool SavedSearchDefaultsToPublic { get; set; }
+
+		public string TrustedIPs { get; set; }
+
+		public NamedArtifact Type { get; set; }
+
+		public string Keywords { get; set; }
+
+		public string Notes { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+
+		public NamedArtifact CreatedBy { get; set; }
+
+		public NamedArtifact LastModifiedBy { get; set; }
+
+		public DateTime LastModifiedOn { get; set; }
+
+		public Meta Meta { get; set; }
+
+		public List<HttpAction> Actions { get; set; }
+
+		public DateTime? LastLoginDate { get; set; }
+	}
+}

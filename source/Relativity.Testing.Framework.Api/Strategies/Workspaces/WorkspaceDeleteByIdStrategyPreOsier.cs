4174dfa576759f57ab96ad90f7da0117c394e848
maria-kizhaeva <82820579+maria-kizhaeva@users.noreply.github.com>
2021-10-08 12:59:05 +0200

    [RTF-1416] Workspace Delete Strategies V1 (#225)

---
+++ source/Relativity.Testing.Framework.Api/Strategies/Workspaces/WorkspaceDeleteByIdStrategyPreOsier.cs
@@ -0,0 +1,37 @@
+﻿using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Models;
+using Relativity.Testing.Framework.Strategies;
+using Relativity.Testing.Framework.Versioning;
+
+namespace Relativity.Testing.Framework.Api.Strategies
+{
+	[VersionRange("<12.1")]
+	internal class WorkspaceDeleteByIdStrategyPreOsier : WorkspaceDeleteByIdAbstractStrategy
+	{
+		private readonly IRestService _restService;
+
+		public WorkspaceDeleteByIdStrategyPreOsier(
+			IRestService restService,
+			IEnsureExistsByIdStrategy<Workspace> ensureExistsByIdStrategy,
+			IWaitDeleteWorkspaceStrategy waitDeleteWorkspaceStrategy)
+			: base(ensureExistsByIdStrategy, waitDeleteWorkspaceStrategy)
+		{
+			_restService = restService;
+		}
+
+		protected override void DeleteWorkspace(int id)
+		{
+			var dto = new
+			{
+				workspace = new
+				{
+					ArtifactID = id
+				}
+			};
+
+			_restService.Post(
+				"Relativity.Services.Workspace.IWorkspaceModule/Workspace Manager Service/DeleteAsync",
+				dto);
+		}
+	}
+}

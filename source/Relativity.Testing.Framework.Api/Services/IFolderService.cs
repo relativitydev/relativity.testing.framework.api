63e5cbeaffff8dddacf17926561dc1de027993e8
klaudiaknafel <81292389+klaudiaknafel@users.noreply.github.com>
2021-10-11 10:29:25 +0200

    Testeng 1576 (#228)
    * [TESTENG] Adding GetWorkspaceRootFolderStrategy.

---
+++ source/Relativity.Testing.Framework.Api/Services/IFolderService.cs
@@ -0,0 +1,28 @@
+﻿using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Api.Services
+{
+	/// <summary>
+	/// Represents the Folder API service.
+	/// </summary>
+	/// <example>
+	/// <code>
+	/// IFolderService _folderService = relativityFacade.Resolve&lt;IFolderService&gt;();
+	/// </code>
+	/// </example>
+	public interface IFolderService
+	{
+		/// <summary>
+		/// Gets the root [Folder](https://relativitydev.github.io/relativity.testing.framework/api/Relativity.Testing.Framework.Models.Folder.html) of the workspace.
+		/// </summary>
+		/// <param name="workspaceArtifactID">The ArtifactID of the workspace.</param>
+		/// <returns>The root [Folder](https://relativitydev.github.io/relativity.testing.framework/api/Relativity.Testing.Framework.Models.Folder.html) of the workspace.</returns>
+		/// <example>
+		/// <code>
+		/// int workspaceArtifactId = 1015427;
+		/// Folder rootFolder = _folderService.Get(workspaceArtifactId);
+		/// </code>
+		/// </example>
+		Folder GetWorkspaceRootFolder(int workspaceArtifactID);
+	}
+}

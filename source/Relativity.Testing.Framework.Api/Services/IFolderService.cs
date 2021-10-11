2feb2a172303f66fecc39f2887b0f4113e63d685
Klaudia Knafel <klaudia.knafel@relativity.com>
2021-10-11 18:09:21 +0200

    [TESTENG-1575] Adding examples to IFolderService.Query

--- source/Relativity.Testing.Framework.Api/Services/IFolderService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IFolderService.cs
@@ -19,6 +19,23 @@ namespace Relativity.Testing.Framework.Api.Services
 		/// <param name="query">The query that may be an empty query or include conditions.</param>
 		/// <param name="length">Indicates the number of returned results. The default value is 0 for length, and the number of returned results defaults to 10000.</param>
 		/// <returns>A [QueryResult](https://relativitydev.github.io/relativity.testing.framework/api/Relativity.Testing.Framework.Models.QueryResult-1.html) with a list of all folders in the workspace that are available to requesting user.</returns>
+		/// <example> This example shows how to query for folder with given Name.
+		/// <code>
+		/// int workspaceArtifactId = 1015427;
+		/// var query = new Query
+		/// {
+		/// 	Condition = "'Name' == 'Test'"
+		/// };
+		/// Folder folderWithTestName = _folderService.Get(workspaceArtifactId, query, 1).Results.FirstOrDefault();
+		/// </code>
+		/// </example>
+		/// <example> This example shows how to query for all folders in the workspace.
+		/// <code>
+		/// int workspaceArtifactId = 1015427;
+		/// var query = new Query();
+		/// QueryResult&lt;NamedArtifact&gt; folders = _folderService.Get(workspaceArtifactId, query)
+		/// </code>
+		/// </example>
 		QueryResult<NamedArtifact> Query(int workspaceArtifactID, Query query, int length = 0);
 
 		/// <summary>

49a4716114a169c01584054cb78baeaed34dc6ae
Klaudia Knafel <klaudia.knafel@relativity.com>
2021-10-11 15:12:34 +0200

    [TESTENG-1575] Adding Query strategy for FolderService.

--- source/Relativity.Testing.Framework.Api/Services/FolderService.cs
+++ source/Relativity.Testing.Framework.Api/Services/FolderService.cs
@@ -5,14 +5,22 @@ namespace Relativity.Testing.Framework.Api.Services
 {
 	internal class FolderService : IFolderService
 	{
+		private readonly IFolderQueryStrategy _queryStrategy;
+
 		private readonly IFolderGetWorkspaceRootFolderStrategy _getWorkspaceRootFolderStrategy;
 
-		public FolderService(IFolderGetWorkspaceRootFolderStrategy getWorkspaceRootFolderStrategy)
+		public FolderService(
+			IFolderQueryStrategy queryStrategy,
+			IFolderGetWorkspaceRootFolderStrategy getWorkspaceRootFolderStrategy)
 		{
+			_queryStrategy = queryStrategy;
 			_getWorkspaceRootFolderStrategy = getWorkspaceRootFolderStrategy;
 		}
 
 		public Folder GetWorkspaceRootFolder(int workspaceArtifactID)
 			=> _getWorkspaceRootFolderStrategy.Get(workspaceArtifactID);
+
+		public QueryResult<NamedArtifact> Query(int workspaceArtifactID, Query query, int length = 0)
+			=> _queryStrategy.Query(workspaceArtifactID, query, length);
 	}
 }
--- source/Relativity.Testing.Framework.Api/Services/IFolderService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IFolderService.cs
@@ -13,6 +13,15 @@ namespace Relativity.Testing.Framework.Api.Services
 	public interface IFolderService
 	{
 		/// <summary>
+		/// Queries for unstructured list of [Folder](https://relativitydev.github.io/relativity.testing.framework/api/Relativity.Testing.Framework.Models.Folder.html)s.
+		/// </summary>
+		/// <param name="workspaceArtifactID">The ArtifactID of the workspace.</param>
+		/// <param name="query">The query that may be an empty query or include conditions.</param>
+		/// <param name="length">Indicates the number of returned results. The default value is 0 for length, and the number of returned results defaults to 10000.</param>
+		/// <returns>A [QueryResult](https://relativitydev.github.io/relativity.testing.framework/api/Relativity.Testing.Framework.Models.QueryResult-1.html) with a list of all folders in the workspace that are available to requesting user.</returns>
+		QueryResult<NamedArtifact> Query(int workspaceArtifactID, Query query, int length = 0);
+
+		/// <summary>
 		/// Gets the root [Folder](https://relativitydev.github.io/relativity.testing.framework/api/Relativity.Testing.Framework.Models.Folder.html) of the workspace.
 		/// </summary>
 		/// <param name="workspaceArtifactID">The ArtifactID of the workspace.</param>

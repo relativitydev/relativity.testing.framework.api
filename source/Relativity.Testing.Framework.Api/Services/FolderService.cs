7a66f6a17acd602ff2e130726dec6b789c01b1da
klaudiaknafel <81292389+klaudiaknafel@users.noreply.github.com>
2021-10-12 11:04:33 +0200

    [TESTENG-1574] Adding DeleteUnused strategy for FolderService. (#230)
    * [TESTENG-1574] Adding DeleteUnused strategy for FolderService.

--- source/Relativity.Testing.Framework.Api/Services/FolderService.cs
+++ source/Relativity.Testing.Framework.Api/Services/FolderService.cs
@@ -5,22 +5,29 @@ namespace Relativity.Testing.Framework.Api.Services
 {
 	internal class FolderService : IFolderService
 	{
+		private readonly IFolderDeleteUnusedStrategy _deleteUnusedStrategy;
+
 		private readonly IFolderQueryStrategy _queryStrategy;
 
 		private readonly IFolderGetWorkspaceRootFolderStrategy _getWorkspaceRootFolderStrategy;
 
 		public FolderService(
+			IFolderDeleteUnusedStrategy deleteUnusedStrategy,
 			IFolderQueryStrategy queryStrategy,
 			IFolderGetWorkspaceRootFolderStrategy getWorkspaceRootFolderStrategy)
 		{
+			_deleteUnusedStrategy = deleteUnusedStrategy;
 			_queryStrategy = queryStrategy;
 			_getWorkspaceRootFolderStrategy = getWorkspaceRootFolderStrategy;
 		}
 
-		public Folder GetWorkspaceRootFolder(int workspaceArtifactID)
-			=> _getWorkspaceRootFolderStrategy.Get(workspaceArtifactID);
+		public QueryResult<Artifact> DeleteUnused(int workspaceArtifactID)
+			=> _deleteUnusedStrategy.Delete(workspaceArtifactID);
 
 		public QueryResult<NamedArtifact> Query(int workspaceArtifactID, Query query, int length = 0)
 			=> _queryStrategy.Query(workspaceArtifactID, query, length);
+
+		public Folder GetWorkspaceRootFolder(int workspaceArtifactID)
+			=> _getWorkspaceRootFolderStrategy.Get(workspaceArtifactID);
 	}
 }

68f9d08b6f1c109f164c8c4aeeefb51f8cc43e03
Dawid Stoga <dawid.stoga@relativity.com>
2021-11-18 22:23:22 +0100

    Relativity.DataExchange.Client.SDK updated to version 1.15.27

--- source/Relativity.Testing.Framework.Api/Kepler/KeplerServiceFactory.cs
+++ source/Relativity.Testing.Framework.Api/Kepler/KeplerServiceFactory.cs
@@ -8,8 +8,6 @@ namespace Relativity.Testing.Framework.Api.Kepler
 	{
 		private readonly Uri _restUri;
 
-		private readonly Uri _rsapiUri;
-
 		private readonly ServiceFactory _defaultFactory;
 
 		public KeplerServiceFactory(IConfigurationService configurationService)
@@ -17,10 +15,6 @@ namespace Relativity.Testing.Framework.Api.Kepler
 			var relativityInstanceSettings = configurationService.RelativityInstance;
 
 			_restUri = new Uri($"{relativityInstanceSettings.ServerBindingType}://{relativityInstanceSettings.RestServicesHostAddress}/relativity.rest/api");
-
-			_rsapiUri = !string.IsNullOrEmpty(relativityInstanceSettings.RsapiServicesHostAddress)
-				? new Uri($"{relativityInstanceSettings.ServerBindingType}://{relativityInstanceSettings.RsapiServicesHostAddress}/relativity.services")
-				: _restUri;
 
 			_defaultFactory = CreateServiceFactory(relativityInstanceSettings.AdminUsername, relativityInstanceSettings.AdminPassword);
 		}
@@ -41,7 +35,7 @@ namespace Relativity.Testing.Framework.Api.Kepler
 		private ServiceFactory CreateServiceFactory(string username, string password)
 		{
 			UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
-			ServiceFactorySettings settings = new ServiceFactorySettings(_rsapiUri, _restUri, credentials);
+			ServiceFactorySettings settings = new ServiceFactorySettings(_restUri, credentials);
 
 			return new ServiceFactory(settings);
 		}

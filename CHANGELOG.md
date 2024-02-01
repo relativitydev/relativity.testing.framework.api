2ab8247ee711be712a852cabada75fd7b86c82fb
Andrew Wells <andrew.wells@relativity.com>
2024-02-01 17:19:19 -0600

    Unimplementing ImportApiService

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [7.0.0] - 2024-02-02
+### Fixed
+- Unimplemented ImportApiService, which previously was implemented by the now-deprecated ImportAPI. This causes most data import strategies to fail.
+
 ## [6.1.9] - 2023-05-22
 
 ### Updated
--- source/Relativity.Testing.Framework.Api/Services/IImportApiService.cs
+++ source/Relativity.Testing.Framework.Api/Services/IImportApiService.cs
@@ -1,11 +1,16 @@
-﻿using kCura.Relativity.ImportAPI;
+﻿using kCura.Relativity.DataReaderClient;
 
 namespace Relativity.Testing.Framework.Api.Services
 {
 	/// <summary>
-	/// Represents the REST HTTP service that provides a set of methods to communicate with Relativity Kepler REST API.
+	/// Represents the REST HTTP service that provides NOTHING.
 	/// </summary>
-	public interface IImportApiService : IImportAPI
+	public interface IImportApiService
 	{
+		ImageImportBulkArtifactJob NewImageImportJob();
+
+		ImportBulkArtifactJob NewNativeDocumentImportJob();
+
+		ImageImportBulkArtifactJob NewProductionImportJob(int productionId);
 	}
 }
